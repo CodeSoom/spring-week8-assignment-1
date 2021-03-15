2c51e64b34da7d8125b92999ca27bc04b4e19a88
JamieShin0201 <jamieshin1450@gmail.com>
2021-03-15 20:16:38 +0900

    회원 컨트롤러, 서비스에 주석을 작성하라
    회원 컨트롤러, 서비스가 어떤 역할을 하는지 설명하기 위해 주석을 작성하였습니다.

--- app/src/main/java/com/codesoom/assignment/controllers/UserController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/UserController.java
@@ -9,10 +9,20 @@ import com.codesoom.assignment.security.UserAuthentication;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+/**
+ * 회원과 관련된 HTTP 요청 처리를 담당합니다.
+ */
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -22,6 +32,12 @@ public class UserController {
         this.userService = userService;
     }
 
+    /**
+     * 주어진 회원 정보로 회원을 생성하고, 생성된 회원을 응답합니다.
+     *
+     * @param registrationData 회원 정보
+     * @return 생성된 회원
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     UserResultData create(@RequestBody @Valid UserRegistrationData registrationData) {
@@ -29,6 +45,16 @@ public class UserController {
         return getUserResultData(user);
     }
 
+    /**
+     * 주어진 id에 해당하는 회원을 주어진 회원 수정 정보로 변경하고,
+     * 변경된 회원을 응답합니다.
+     *
+     * @param id 변경하고자 하는 회원 식별자
+     * @param modificationData 회원 수정 정보
+     * @param authentication 인증된 회원
+     * @return 변경된 회원
+     * @throws AccessDeniedException 변경하려고 하는 회원이 자기 자신이 아닐 경우
+     */
     @PatchMapping("{id}")
     @PreAuthorize("isAuthenticated() and hasAuthority('USER')")
     UserResultData update(
@@ -41,6 +67,11 @@ public class UserController {
         return getUserResultData(user);
     }
 
+    /**
+     * 주어진 id에 해당하는 회원을 삭제합니다.
+     *
+     * @param id 회원 식별자
+     */
     @DeleteMapping("{id}")
     @PreAuthorize("isAuthenticated() and hasAuthority('ADMIN')")
     void destroy(@PathVariable Long id) {
