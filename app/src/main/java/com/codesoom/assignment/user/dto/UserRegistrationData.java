f8a9072590185f7cf4f9abff9c7346a766206efb
etff <wish2rich@gmail.com>
2021-03-15 22:53:32 +0900

    fix(user): add no argument constructor
    사용자 생성, 수정시 사용되는 DTO의 인자 없는 생성자를 추가하여
    Jackson을 이용하여 serialize 하는 도중 발생하는 에러를 제거하였습니다.

--- app/src/main/java/com/codesoom/assignment/user/dto/UserRegistrationData.java
+++ app/src/main/java/com/codesoom/assignment/user/dto/UserRegistrationData.java
@@ -4,6 +4,7 @@ import com.github.dozermapper.core.Mapping;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -11,6 +12,7 @@ import javax.validation.constraints.Size;
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserRegistrationData {
     @NotBlank
     @Size(min = 3)
