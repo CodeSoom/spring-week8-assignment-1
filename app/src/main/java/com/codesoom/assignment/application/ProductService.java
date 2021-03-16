abe8433a38fbc8bee7066961318020775ddaa4c3
newoo <newoo4297@naver.com>
2021-03-16 20:52:21 +0900

    제품 서비스 JavaDoc을 영어에서 한국어로 변경

--- app/src/main/java/com/codesoom/assignment/application/ProductService.java
+++ app/src/main/java/com/codesoom/assignment/application/ProductService.java
@@ -11,7 +11,7 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 /**
- * Service for products.
+ * 장난감 서비스
  *
  * @author newoo (newoo4297@naver.com)
  */
@@ -30,30 +30,30 @@ public class ProductService {
     }
 
     /**
-     * Returns all products in this application.
+     * 애플리케이션에 있는 전체 장난감들을 반환한다.
      *
-     * @return all products.
+     * @return 전체 장난감들
      */
     public List<Product> getProducts() {
         return productRepository.findAll();
     }
 
     /**
-     * Returns the products with given ID.
+     * 주어진 식별자를 가지고 있는 장난감을 반환한다.
      *
-     * @param id is identifier of the product.
-     * @return the products with given ID.
-     * @throws ProductNotFoundException in case any product doesn't exist with given ID.
+     * @param id 장난감 식별자.
+     * @return 주어진 식별자를 가지고 있는 장난감.
+     * @throws ProductNotFoundException 주어진 식별자를 가진 장난감이 없다는 예외.
      */
     public Product getProduct(Long id) {
         return findProduct(id);
     }
 
     /**
-     * Returns the saved product.
+     * 애플리케이션에 장난감을 저장한다.
      *
-     * @param productData is given data to save a product in this application.
-     * @return the saved product.
+     * @param productData 애플리케이션에 저정할 장난감 데이터.
+     * @return 저장된 장난감.
      */
     public Product createProduct(ProductData productData) {
         Product product = mapper.map(productData, Product.class);
@@ -61,12 +61,12 @@ public class ProductService {
     }
 
     /**
-     * Returns the modified product with given ID.
+     * 주어진 식별자를 가지고, 수정된 장난감을 반환한다.
      *
-     * @param id is identifier of the product to modify.
-     * @param productData is given data to modify the product with given ID.
-     * @return the modified product.
-     * @throws ProductNotFoundException in case any product doesn't exist with given ID.
+     * @param id 수정할 장난감의 식별자.
+     * @param productData 애플리케이션에 수정할 장난감 데이터.
+     * @return 수정된 장난감
+     * @throws ProductNotFoundException 주어진 식별자를 가진 장난감이 없다는 예외.
      */
     public Product updateProduct(Long id, ProductData productData) {
         Product product = findProduct(id);
@@ -77,10 +77,10 @@ public class ProductService {
     }
 
     /**
-     * Returns the removed product in this application.
+     * 애플리케이션에서 삭제된 장난감을 반환한다.
      *
-     * @param id is identifier of the product to remove
-     * @return the removed product.
+     * @param id 삭제할 장난감의 식별자.
+     * @return 삭제된 장난감
      */
     public Product deleteProduct(Long id) {
         Product product = findProduct(id);
