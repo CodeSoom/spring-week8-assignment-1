3b2c896b9e039bd3a3761c47a4c45fea4ad01b07
etff <wish2rich@gmail.com>
2021-03-16 11:32:14 +0900

    doc(product): add product javadoc
    product 영역에 javadoc을 작성합니다.

--- app/src/main/java/com/codesoom/assignment/product/dto/ProductData.java
+++ app/src/main/java/com/codesoom/assignment/product/dto/ProductData.java
@@ -10,26 +10,34 @@ import lombok.Setter;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+/**
+ * 상품 명세서.
+ */
 @Setter
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductData {
+    /** 상품 식별자. */
     private Long id;
 
+    /** 상품명. */
     @NotBlank
     @Mapping("name")
     private String name;
 
+    /** 상품제조사. */
     @NotBlank
     @Mapping("maker")
     private String maker;
 
+    /** 상품가격. */
     @NotNull
     @Mapping("price")
     private Integer price;
 
+    /** 상품 이미지. */
     @Mapping("imageUrl")
     private String imageUrl;
 }
