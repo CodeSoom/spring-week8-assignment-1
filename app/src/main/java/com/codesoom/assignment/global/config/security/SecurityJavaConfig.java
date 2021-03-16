42394b87a60126d73b3ba55b228ef07cabec262a
etff <wish2rich@gmail.com>
2021-03-16 14:17:34 +0900

    refactor(security): remove character encoding filter
    Character Filter를 생성하지 않고 yml 의 encoding의 encoding 설정을 통해
    불필요한 객체 생성을 제거하였습니다.
    
    see also
    > https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#server.servlet.encoding.charset

--- app/src/main/java/com/codesoom/assignment/global/config/security/SecurityJavaConfig.java
+++ app/src/main/java/com/codesoom/assignment/global/config/security/SecurityJavaConfig.java
@@ -2,7 +2,6 @@ package com.codesoom.assignment.global.config.security;
 
 import com.codesoom.assignment.auth.application.AuthenticationService;
 import com.codesoom.assignment.global.config.security.filters.AuthenticationErrorFilter;
-import com.codesoom.assignment.global.config.security.filters.CharacterFilter;
 import com.codesoom.assignment.global.config.security.filters.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -30,10 +29,8 @@ public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
         Filter authenticationFilter = new JwtAuthenticationFilter(
                 authenticationManager(), authenticationService);
         Filter authenticationErrorFilter = new AuthenticationErrorFilter();
-        CharacterFilter characterFilter = new CharacterFilter();
 
         http
-                .addFilterBefore(characterFilter.getFilter(), CsrfFilter.class)
                 .csrf().disable()
                 .headers()
                 .frameOptions().disable()
