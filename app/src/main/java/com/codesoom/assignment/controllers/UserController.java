13d5c5e4c6cdc9217a0d54e25955cf173bd7b113
seungin yang <rhfpdk92@naver.com>
2021-03-17 00:00:32 +0900

    Add javadoc to UserController

--- app/src/main/java/com/codesoom/assignment/controllers/UserController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/UserController.java
@@ -5,14 +5,25 @@ import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.dto.UserModificationData;
 import com.codesoom.assignment.dto.UserRegistrationData;
 import com.codesoom.assignment.dto.UserResultData;
+import com.codesoom.assignment.errors.UserNotFoundException;
 import com.codesoom.assignment.security.UserAuthentication;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+/**
+ * 회원과 관련된 HTTP요청을 처리합니다.
+ */
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -22,6 +33,12 @@ public class UserController {
         this.userService = userService;
     }
 
+    /**
+     * 회원을 저장하고 저장된 회원 정보를 반환합니다.
+     *
+     * @param registrationData 저장할 회원 정보
+     * @return 저장된 회원 정보
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     UserResultData create(@RequestBody @Valid UserRegistrationData registrationData) {
@@ -29,6 +46,16 @@ public class UserController {
         return getUserResultData(user);
     }
 
+    /**
+     * id에 해당하는 회원을 수정하고 수정된 회원 정보를 반환합니다.
+     * 인증되고 USER권한을 가진 사용자만 수정할 수 있습니다.
+     *
+     * @param id               수정할 회원의 id
+     * @param modificationData 수정할 회원 정보
+     * @param authentication   현재 유저의 인증
+     * @return 수정된 회원의 정보
+     * @throws AccessDeniedException 현재 유저가 아닌 회원의 정보를 수정하는 경우
+     */
     @PatchMapping("{id}")
     @PreAuthorize("isAuthenticated() and hasAuthority('USER')")
     UserResultData update(
@@ -41,6 +68,13 @@ public class UserController {
         return getUserResultData(user);
     }
 
+    /**
+     * id에 해당하는 회원을 삭제합니다.
+     * 인증되고 ADMIN권한을 가진 사용자만 삭제할 수 있습니다.
+     *
+     * @param id 삭제할 회원의 id
+     * @throws UserNotFoundException 삭제할 회원이 없거나 이미 삭제된 경우
+     */
     @DeleteMapping("{id}")
     @PreAuthorize("isAuthenticated() and hasAuthority('ADMIN')")
     void destroy(@PathVariable Long id) {
