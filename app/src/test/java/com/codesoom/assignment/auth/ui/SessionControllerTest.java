40ea82c7c5a5f4df7f4436353ec879b4b5538e18
etff <wish2rich@gmail.com>
2021-03-16 19:28:40 +0900

    doc(auth): add authentication rest api doc
    로그인 인증 REST API 문서 작성

--- app/src/test/java/com/codesoom/assignment/auth/ui/SessionControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/auth/ui/SessionControllerTest.java
@@ -2,21 +2,38 @@ package com.codesoom.assignment.auth.ui;
 
 import com.codesoom.assignment.auth.application.AuthenticationService;
 import com.codesoom.assignment.auth.application.LoginFailException;
+import com.codesoom.assignment.common.RestDocConfiguration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(SessionController.class)
+@Import(RestDocConfiguration.class)
+@AutoConfigureRestDocs
 class SessionControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -39,13 +56,26 @@ class SessionControllerTest {
     @Test
     void loginWithRightEmailAndPassword() throws Exception {
         mockMvc.perform(
-                post("/session")
+                RestDocumentationRequestBuilders.
+                        post("/session")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"email\":\"tester@example.com\"," +
                                 "\"password\":\"test\"}")
         )
                 .andExpect(status().isCreated())
-                .andExpect(content().string(containsString(".")));
+                .andExpect(content().string(containsString(".")))
+                .andDo(print())
+                .andDo(document("create-session",
+                        requestFields(
+                                fieldWithPath("email").type(JsonFieldType.STRING).description("이메일").optional()
+                                        .attributes(key("constraints").value("")),
+                                fieldWithPath("password").type(JsonFieldType.STRING).description("비밀번호").optional()
+                                        .attributes(key("constraints").value(""))
+                        ),
+                        responseFields(
+                                fieldWithPath("accessToken").type(JsonFieldType.STRING).description("응답 토큰")
+                        ))
+                );
     }
 
     @Test
