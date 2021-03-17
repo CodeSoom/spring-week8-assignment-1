8c7fbf8895689af9045f381853239966992c7fc6
dperformance <docyoungs@naver.com>
2021-03-17 13:06:59 +0900

    인증 컨트롤러와 서비스의 역할을 설명하라

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -12,6 +12,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * 인증과 관련된 비즈니스 로직을 담당합니다.
+ */
 @Service
 public class AuthenticationService {
     private final UserRepository userRepository;
@@ -29,6 +32,14 @@ public class AuthenticationService {
         this.passwordEncoder = passwordEncoder;
     }
 
+    /**
+     * 로그인정보를 확인하고 Access token 을 반환.
+     *
+     * @param email 이메일.
+     * @param password 비밀번호.
+     * @return Access token.
+     * @throws LoginFailException 로그인 정보가 다를 경우.
+     */
     public String login(String email, String password) {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new LoginFailException(email));
@@ -40,11 +51,23 @@ public class AuthenticationService {
         return jwtUtil.encode(user.getId());
     }
 
+    /**
+     * 회원 id를 찾아 반환.
+     *
+     * @param accessToken 토큰.
+     * @return 회원 id.
+     */
     public Long parseToken(String accessToken) {
         Claims claims = jwtUtil.decode(accessToken);
         return claims.get("userId", Long.class);
     }
 
+    /**
+     * 회원이 가진 권한을 반환.
+     *
+     * @param userId 회원 id.
+     * @return 권한.
+     */
     public List<Role> roles(Long userId) {
         return roleRepository.findAllByUserId(userId);
     }
--- app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
@@ -3,9 +3,14 @@ package com.codesoom.assignment.controllers;
 import com.codesoom.assignment.application.AuthenticationService;
 import com.codesoom.assignment.dto.SessionRequestData;
 import com.codesoom.assignment.dto.SessionResponseData;
+import com.codesoom.assignment.errors.LoginFailException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * 회원 인증과 관련된 HTTP 요청을 처리 합니다.
+ *
+ */
 @RestController
 @RequestMapping("/session")
 public class SessionController {
@@ -15,6 +20,13 @@ public class SessionController {
         this.authenticationService = authenticationService;
     }
 
+    /**
+     * 로그인정보를 확인하고 Access token 을 반환.
+     *
+     * @param sessionRequestData 로그인 정보.
+     * @return Access token.
+     * @throws LoginFailException 로그인 정보가 다를 경우.
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public SessionResponseData login(
