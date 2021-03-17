742e8e7e61232a93249edd30ce754c86724f51fc
양효주 <yhyojoo@icloud.com>
2021-03-18 00:09:26 +0900

    Product에 JavaDoc 주석을 작성하라

--- app/src/main/java/com/codesoom/assignment/domain/Product.java
+++ app/src/main/java/com/codesoom/assignment/domain/Product.java
@@ -1,35 +1,55 @@
 package com.codesoom.assignment.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@Entity
+/**
+ * 상품 정보.
+ */
 @Getter
-@Builder
+@Setter
 @NoArgsConstructor
-@AllArgsConstructor
+@Entity
 public class Product {
     @Id
     @GeneratedValue
     private Long id;
 
+    @Setter
     private String name;
 
+    @Setter
     private String maker;
 
+    @Setter
     private Integer price;
 
-    private String imageUrl;
+    @Setter
+    private String image;
 
-    public void changeWith(Product source) {
+    @Builder
+    public Product(String name, String maker, Integer price, String image) {
+        this.name = name;
+        this.maker = maker;
+        this.price = price;
+        this.image = image;
+    }
+
+    /**
+     * 상품 정보를 수정합니다.
+     *
+     * @param source 수정할 상품 정보
+     */
+    public void updateWith(Product source) {
         this.name = source.name;
         this.maker = source.maker;
         this.price = source.price;
+        this.image = source.image;
     }
 }
