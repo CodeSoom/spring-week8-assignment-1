e883da295ac875d8edb1f50da6353cb55ced641f
newoo <newoo4297@naver.com>
2021-03-18 01:12:06 +0900

    인증 서비스 JavaDoc 추가

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -12,6 +12,11 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * 인증 서비스
+ *
+ * @author newoo (newoo4297@naver.com)
+ */
 @Service
 public class AuthenticationService {
     private final UserRepository userRepository;
@@ -29,6 +34,14 @@ public class AuthenticationService {
         this.passwordEncoder = passwordEncoder;
     }
 
+    /**
+     * 애플리케이션에 로그인 한다.
+     *
+     * @param email 로그인할 계정 이메일
+     * @param password 로그인할 계정 비밀번호
+     * @return 전체 장난감들
+     * @throws LoginFailException 로그인 실패 예외
+     */
     public String login(String email, String password) {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new LoginFailException(email));
@@ -40,11 +53,23 @@ public class AuthenticationService {
         return jwtUtil.encode(user.getId());
     }
 
+    /**
+     * 토큰을 복호화하고, 계정 아이디로 반환한다.
+     *
+     * @param accessToken 인증 토큰
+     * @return 복호화된 유저 아이디
+     */
     public Long parseToken(String accessToken) {
         Claims claims = jwtUtil.decode(accessToken);
         return claims.get("userId", Long.class);
     }
 
+    /**
+     * 유저 권한을 반환한다.
+     *
+     * @param userId 유저 식별자
+     * @return 주어진 유저 식별자를 가진 계정 권한 목록
+     */
     public List<Role> roles(Long userId) {
         return roleRepository.findAllByUserId(userId);
     }
