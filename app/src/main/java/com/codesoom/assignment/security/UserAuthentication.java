6e96f2f57b71c02e79b766fda1eaf57ba0e3d4fd
양효주 <yhyojoo@icloud.com>
2021-03-19 00:11:44 +0900

    UserAuthentication에 JavaDoc 주석을 작성하라

--- app/src/main/java/com/codesoom/assignment/security/UserAuthentication.java
+++ app/src/main/java/com/codesoom/assignment/security/UserAuthentication.java
@@ -8,41 +8,66 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * 사용자 인증 정보.
+ */
 public class UserAuthentication extends AbstractAuthenticationToken {
     private final Long userId;
 
     public UserAuthentication(Long userId, List<Role> roles) {
-        super(authorities(roles));
+        super(authorities(userId, roles));
         this.userId = userId;
     }
 
+    /**
+     * 사용자 자격 정보를 반환합니다.
+     *
+     * @return 사용자 자격 정보
+     */
     @Override
     public Object getCredentials() {
         return null;
     }
 
+    /**
+     * 사용자 인증 객체를 반환합니다.
+     *
+     * @return 사용자 인증 객체
+     */
     @Override
     public Object getPrincipal() {
         return userId;
     }
 
+    /**
+     * 인증된 사용자라면 true를 반환합니다.
+     */
     @Override
     public boolean isAuthenticated() {
         return true;
     }
 
+    /**
+     * 사용자 식별자를 반환합니다.
+     *
+     * @return 사용자 식별자
+     */
     public Long getUserId() {
         return userId;
     }
 
     @Override
     public String toString() {
-        return "Authentication(" + userId + ")";
+        return "Authentication{" + userId + "}";
     }
 
-    private static List<GrantedAuthority> authorities(List<Role> roles) {
-        return roles.stream()
-                .map(role -> new SimpleGrantedAuthority(role.getName()))
-                .collect(Collectors.toList());
+    /**
+     * 인증된 사용자의 권한 목록을 반환합니다.
+     */
+    private static List<GrantedAuthority> authorities(
+            Long userId, List<Role> roles) {
+       return roles.stream()
+               .map(role -> new SimpleGrantedAuthority(role.getName()))
+               .collect(Collectors.toList());
     }
 }
