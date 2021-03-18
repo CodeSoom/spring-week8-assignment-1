a779cbb846a5101c4b5007ee31640f00b83b1772
양효주 <yhyojoo@icloud.com>
2021-03-18 21:27:26 +0900

    UserCreateRequestDto에 JavaDoc 주석을 작성하라

---
+++ app/src/main/java/com/codesoom/assignment/dto/UserCreateRequestDto.java
@@ -0,0 +1,34 @@
+package com.codesoom.assignment.dto;
+
+import com.github.dozermapper.core.Mapping;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * 사용자 등록 요청 정보.
+ */
+@Getter
+@NoArgsConstructor
+public class UserCreateRequestDto {
+    @NotBlank
+    @Mapping("name")
+    private String name;
+
+    @NotBlank
+    @Mapping("email")
+    private String email;
+
+    @NotBlank
+    @Mapping("password")
+    private String password;
+
+    @Builder
+    public UserCreateRequestDto(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
