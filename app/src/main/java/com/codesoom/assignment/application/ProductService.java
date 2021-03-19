e45b2e9bad0c949841c0ba049fcb6cad7c929418
mikekang47 <gyeong0308@gmail.com>
2021-03-19 15:21:35 +0900

    ProductService의 method에 관한 javadoc작성

--- app/src/main/java/com/codesoom/assignment/application/ProductService.java
+++ app/src/main/java/com/codesoom/assignment/application/ProductService.java
@@ -28,19 +28,41 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
+    /**
+     * 저장된 모든 상품을 리턴합니다.
+     * @return 저장된 모든 상품
+     */
     public List<Product> getProducts() {
         return productRepository.findAll();
     }
 
+    /**
+     * 전달된 id에 해당하는 상품을 리턴합니다.
+     * @param id 상품 id
+     * @return id에 해당하는 상품
+     * @throws ProductNotFoundException id에 해당하는 상품을 찾을 수 없는 경우
+     */
     public Product getProduct(Long id) {
         return findProduct(id);
     }
 
+    /**
+     * 절달된 상품 정보로 상품을 생성하고, 그 상품을 리턴합니다.
+     * @param productData 상품 정
+     * @return 생성된 상품
+     */
     public Product createProduct(ProductData productData) {
         Product product = mapper.map(productData, Product.class);
         return productRepository.save(product);
     }
 
+    /**
+     * 전달된 id에 해당하는 상품을 찾고, 전달된 상품 정보를 수정한 후, 수정 된 상품을 리턴합니다.
+     * @param id
+     * @param productData 수정할 상품 정보
+     * @return 수정된 상품
+     * @throws ProductNotFoundException 주어진 id에 해당하는 상품을 찾을 수 없는 경우
+     */
     public Product updateProduct(Long id, ProductData productData) {
         Product product = findProduct(id);
 
@@ -49,6 +71,11 @@ public class ProductService {
         return product;
     }
 
+    /**
+     * 전달된 Id에 해당하는 상품을 삭제합니다.
+     * @param id
+     * @throws ProductNotFoundException 주어진 id에 해당하는 상품을 찾을 수 없는 경우
+     */
     public Product deleteProduct(Long id) {
         Product product = findProduct(id);
 
@@ -57,6 +84,11 @@ public class ProductService {
         return product;
     }
 
+    /**
+     * 전달된 id에 해당하는 상품을 찾습니다.
+     * @param id
+     * @throws ProductNotFoundException 상품을 찾을 수 없을 경우
+     */
     private Product findProduct(Long id) {
         return productRepository.findById(id)
                 .orElseThrow(() -> new ProductNotFoundException(id));
