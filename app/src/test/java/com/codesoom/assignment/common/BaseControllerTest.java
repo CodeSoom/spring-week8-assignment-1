72cf1ce77510082e124a7dccd07a0fed55dade28
etff <wish2rich@gmail.com>
2021-03-20 22:39:46 +0900

    fix: add active profile
    build 시 profile 오류 수정

--- app/src/test/java/com/codesoom/assignment/common/BaseControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/common/BaseControllerTest.java
@@ -6,11 +6,13 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
+@ActiveProfiles("test")
 @Import(RestDocConfiguration.class)
 public class BaseControllerTest {
 
