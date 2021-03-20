430442a0d1b5d2d11bc812648c93eb39662ec34c
Lee Dong Wook <dwlee@crscube.io>
2021-03-20 18:22:34 +0900

    DOCS: add docs of ControllerErrorAdvice

--- app/src/main/java/com/codesoom/assignment/controllers/ControllerErrorAdvice.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ControllerErrorAdvice.java
@@ -11,27 +11,47 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * 컨트롤러의 예외처리를 담당합니다.
+ */
 @ResponseBody
 @ControllerAdvice
 public class ControllerErrorAdvice {
+
+    /**
+     * 상품이 존재하지 않을 때의 예외 처리입니다.
+     * @return 에러 응답
+     */
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ProductNotFoundException.class)
     public ErrorResponse handleProductNotFound() {
         return new ErrorResponse("Product not found");
     }
 
+    /**
+     * 유저가 존재하지 않을 때의 예외 처리입니다.
+     * @return 에러 응답
+     */
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(UserNotFoundException.class)
     public ErrorResponse handleUserNotFound() {
         return new ErrorResponse("User not found");
     }
 
+    /**
+     * 유저 이메일이 중복될 때의 예외 처리입니다.
+     * @return 에러 응답
+     */
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(UserEmailDuplicationException.class)
     public ErrorResponse handleUserEmailIsAlreadyExisted() {
         return new ErrorResponse("User's email address is already existed");
     }
 
+    /**
+     * 로그인 실패시 예외 처리입니다.
+     * @return 에러 응답
+     */
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(LoginFailException.class)
     public ErrorResponse handleLoginFailException() {
