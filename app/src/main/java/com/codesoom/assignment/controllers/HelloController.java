7551fe439a06515c85525c315ce37e54cf32c189
Lee Dong Wook <dwlee@crscube.io>
2021-03-20 18:25:27 +0900

    DOCS: add docs of HelloController

--- app/src/main/java/com/codesoom/assignment/controllers/HelloController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/HelloController.java
@@ -3,8 +3,15 @@ package com.codesoom.assignment.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 인사 관련 요청을 처리합니다.
+ */
 @RestController
 public class HelloController {
+    /**
+     * 인사를 요청하면 인사말을 반환한다.
+     * @return "Hello, world!"
+     */
     @RequestMapping("/")
     public String sayHello() {
         return "Hello, world!";
