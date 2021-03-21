a972447577877f4ca14bfb73ca784de8d8baafdb
newoo <newoo4297@naver.com>
2021-03-21 15:23:41 +0900

    장난감 서비스의 JavaDoc을 수정

--- app/src/main/java/com/codesoom/assignment/application/ProductService.java
+++ app/src/main/java/com/codesoom/assignment/application/ProductService.java
@@ -30,29 +30,29 @@ public class ProductService {
     }
 
     /**
-     * 애플리케이션에 있는 전체 장난감들을 반환한다.
+     * 전체 장난감들을 반환한다.
      *
-     * @return 전체 장난감들
+     * @return 전체 장난감들.
      */
     public List<Product> getProducts() {
         return productRepository.findAll();
     }
 
     /**
-     * 주어진 식별자를 가지고 있는 장난감을 반환한다.
+     * 주어진 식별자에 해당하는 장난감을 반환한다.
      *
      * @param id 장난감 식별자.
-     * @return 주어진 식별자를 가지고 있는 장난감.
-     * @throws ProductNotFoundException 주어진 식별자를 가진 장난감이 없다는 예외.
+     * @return 주어진 식별자에 해당하는 장난감.
+     * @throws ProductNotFoundException 주어진 식별자에 해당하는 장난감이 없을 경우.
      */
     public Product getProduct(Long id) {
         return findProduct(id);
     }
 
     /**
-     * 애플리케이션에 장난감을 저장한다.
+     * 주어진 데이터로 장난감을 생성한다.
      *
-     * @param productData 애플리케이션에 저정할 장난감 데이터.
+     * @param productData 저정할 장난감 데이터.
      * @return 저장된 장난감.
      */
     public Product createProduct(ProductData productData) {
@@ -61,12 +61,12 @@ public class ProductService {
     }
 
     /**
-     * 주어진 식별자를 가지고, 수정된 장난감을 반환한다.
+     * 주어진 식별자에 해당하는 장난감을 수정하고, 수정된 장난감을 반환한다.
      *
      * @param id 수정할 장난감의 식별자.
-     * @param productData 애플리케이션에 수정할 장난감 데이터.
-     * @return 수정된 장난감
-     * @throws ProductNotFoundException 주어진 식별자를 가진 장난감이 없다는 예외.
+     * @param productData 수정할 장난감 데이터.
+     * @return 수정된 장난감.
+     * @throws ProductNotFoundException 주어진 식별자에 해당하는 장난감이 없을 경우.
      */
     public Product updateProduct(Long id, ProductData productData) {
         Product product = findProduct(id);
@@ -77,10 +77,11 @@ public class ProductService {
     }
 
     /**
-     * 애플리케이션에서 삭제된 장난감을 반환한다.
+     * 주어진 식별자에 해당하는 장난감을 삭제하고, 삭제된 장난감을 반환한다.
      *
      * @param id 삭제할 장난감의 식별자.
-     * @return 삭제된 장난감
+     * @return 삭제된 장난감.
+     * @throws ProductNotFoundException 주어진 식별자에 해당하는 장난감이 없을 경우.
      */
     public Product deleteProduct(Long id) {
         Product product = findProduct(id);
@@ -90,6 +91,13 @@ public class ProductService {
         return product;
     }
 
+    /**
+     * 주어진 식별자에 해당하는 장난감을 찾고, 찾은 장난감을 반환한다.
+     *
+     * @param id 찾을 장난감의 식별자.
+     * @return 주어진 식별자에 해당하는 장난감.
+     * @throws ProductNotFoundException 주어진 식별자에 해당하는 장난감이 없을 경우.
+     */
     private Product findProduct(Long id) {
         return productRepository.findById(id)
                 .orElseThrow(() -> new ProductNotFoundException(id));
