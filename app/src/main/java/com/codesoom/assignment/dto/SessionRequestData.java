1d2564cae79e633371b0eccfd3bfa7fd1ae0d767
양효주 <yhyojoo@icloud.com>
2021-03-23 23:46:09 +0900

    컴파일 에러를 해결하라

--- app/src/main/java/com/codesoom/assignment/dto/SessionRequestData.java
+++ app/src/main/java/com/codesoom/assignment/dto/SessionRequestData.java
@@ -1,9 +1,33 @@
 package com.codesoom.assignment.dto;
 
+import com.github.dozermapper.core.Mapping;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
+/**
+ * 세션 요청 DTO.
+ */
 @Getter
+@NoArgsConstructor
 public class SessionRequestData {
+    @NotBlank
+    @Mapping("email")
     private String email;
+
+    @NotBlank
+    @Mapping("password")
     private String password;
+
+    @Builder
+    public SessionRequestData(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public boolean authenticate(String password) {
+        return password.equals(this.password);
+    }
 }
