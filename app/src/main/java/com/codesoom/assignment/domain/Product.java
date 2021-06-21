359a13f1822fe4a8ff413b9966459d1b39d253df
youdongkwan <dkyou7@naver.com>
2021-06-21 17:14:18 +0900

    [Docs] Product.java, ProductController.java, Productdata.java, ProductService.java 에 javaDoc 을 추가하였습니다.

--- app/src/main/java/com/codesoom/assignment/domain/Product.java
+++ app/src/main/java/com/codesoom/assignment/domain/Product.java
@@ -9,6 +9,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+/**
+ * 상품 정보
+ */
 @Entity
 @Getter
 @Builder
@@ -27,6 +30,11 @@ public class Product {
 
     private String imageUrl;
 
+    /**
+     * 상품 정보를 변경합니다.
+     *
+     * @param source 변경할 상품 정보
+     */
     public void changeWith(Product source) {
         this.name = source.name;
         this.maker = source.maker;
