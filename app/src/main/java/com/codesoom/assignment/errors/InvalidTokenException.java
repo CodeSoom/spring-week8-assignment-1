eeba42b2c0e17e81767dc417832cebe9e2d5241d
youdongkwan <dkyou7@naver.com>
2021-06-22 10:11:04 +0900

    [Docs] 에러 처리 관련 클래스에 주석을 추가했습니다. - ControllerErrorAdvice.java - ErrorResponse.java - InvalidTokenException.java - LoginFailException.java - ProductNotFoundException.java

--- app/src/main/java/com/codesoom/assignment/errors/InvalidTokenException.java
+++ app/src/main/java/com/codesoom/assignment/errors/InvalidTokenException.java
@@ -1,5 +1,8 @@
 package com.codesoom.assignment.errors;
 
+/**
+ * 잘못된 접근 예외.
+ */
 public class InvalidTokenException extends RuntimeException {
     public InvalidTokenException(String token) {
         super("Invalid token: " + token);
