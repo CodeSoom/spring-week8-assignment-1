b3748a311814d56b8b5800f5960a46a85bc8ae0c
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2021-06-23 21:44:51 +0900

    애플리케이션 sayHello API에 대한 문서 작성.
    - 클래스 테스트에서 스프링 WebMvc 테스트로 변경.
    - HelloController 테스트에 `@AutoConfigureRestDocs` 적용.
      - GET / 에 대한 문서 작성.
      - 고양이 장난감 가게 API 서버가 가동되고 있음을 알려주는 인사말을 반환합니다.

--- app/src/test/java/com/codesoom/assignment/controllers/HelloControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/HelloControllerTest.java
@@ -1,15 +1,33 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.application.AuthenticationService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@WebMvcTest(HelloController.class)
+@AutoConfigureRestDocs
 class HelloControllerTest {
-    @Test
-    void sayHello() {
-        HelloController controller = new HelloController();
+    @Autowired
+    private MockMvc mockMvc;
 
-        assertThat(controller.sayHello()).isEqualTo("Hello, world!");
+    @MockBean
+    private AuthenticationService authenticationService;
+
+    @Test
+    void sayHello() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, world!")))
+                .andDo(document("app-say-hello"));
     }
 }
