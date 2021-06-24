2a72486b71d482ed082b018736c7c3c1260aaaf3
sedin <sedin@kakao.com>
2021-06-24 14:23:04 +0900

    Feat: 컨트롤러에 JavaDoc 추가
    - 각 컨트롤러에 JavaDoc 작성

--- app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
@@ -4,8 +4,15 @@ import com.codesoom.assignment.application.AuthenticationService;
 import com.codesoom.assignment.dto.SessionRequestData;
 import com.codesoom.assignment.dto.SessionResponseData;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 로그인 관련 http 요청 처리를 담당합니다.
+ */
 @RestController
 @RequestMapping("/session")
 public class SessionController {
@@ -15,6 +22,11 @@ public class SessionController {
         this.authenticationService = authenticationService;
     }
 
+    /**
+     * 로그인 요청을 받아 엑세스 토큰을 리턴합니다.
+     * @param sessionRequestData 로그인을 위한 사용자 정보
+     * @return 엑세스 토큰
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public SessionResponseData login(
