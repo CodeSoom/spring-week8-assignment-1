49d92d1feb4c264f05636ba9a52f5256f2b660c2
youdongkwan <dkyou7@naver.com>
2021-06-25 15:14:27 +0900

    [Feature] 요청 / 응답 본문 문서화하기 위한 환경설정을 추가하였습니다. - RestDocsConfiguration.java

---
+++ app/src/test/java/com/codesoom/assignment/RestDocsConfiguration.java
@@ -0,0 +1,27 @@
+package com.codesoom.assignment;
+
+import org.springframework.boot.test.autoconfigure.restdocs.RestDocsMockMvcConfigurationCustomizer;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentationConfigurer;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+/**
+ * 요청 / 응답 본문 문서화하기 위한 환경설정.
+ */
+@TestConfiguration
+public class RestDocsConfiguration {
+
+    @Bean
+    public RestDocsMockMvcConfigurationCustomizer restDocsMockMvcConfigurationCustomizer(){
+        return new RestDocsMockMvcConfigurationCustomizer() {
+            @Override
+            public void customize(MockMvcRestDocumentationConfigurer configurer) {
+                configurer.operationPreprocessors()
+                        .withRequestDefaults(prettyPrint())
+                        .withResponseDefaults(prettyPrint());
+            }
+        };
+    }
+}
