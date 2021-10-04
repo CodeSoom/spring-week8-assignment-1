5b9770577fdfe5be1ad5cd52da97f1d3c547a286
Sungeheon <elon4856@gmail.com>
2021-10-04 16:45:44 +0900

    Docs: UserService에 javadoc을 작성하세요

--- app/src/main/java/com/codesoom/assignment/application/UserService.java
+++ app/src/main/java/com/codesoom/assignment/application/UserService.java
@@ -15,6 +15,9 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
+/**
+ * 유저에 관한 비즈니스 로직을 제공한다.
+ */
 @Service
 @Transactional
 public class UserService {
@@ -33,6 +36,13 @@ public class UserService {
         this.passwordEncoder = passwordEncoder;
     }
 
+    /**
+     * 유저 정보로 유저를 생성 후 반환한다.
+     *
+     * @param registrationData 유저 정보
+     * @return 생성한 유저
+     * @throws UserEmailDuplicationException 유저 정보에 있는 이메일이 이미 저장소에 있는 경우
+     */
     public User registerUser(UserRegistrationData registrationData) {
         String email = registrationData.getEmail();
         if (userRepository.existsByEmail(email)) {
@@ -49,6 +59,15 @@ public class UserService {
         return user;
     }
 
+    /**
+     * 수정 대상 유저 식별자에 해당하는 유저를 전달받은 유저정보로 수정후 반환한다.
+     *
+     * @param id 수정 대상 유저 식별자
+     * @param modificationData 수정 유저 정보
+     * @param userId 수정 요청 유저 식별자
+     * @return 수정된 유저
+     * @throws AccessDeniedException 수정 대상 유저 식별자와 수정 요청 유저 식별자가 다른 경우
+     */
     public User updateUser(Long id, UserModificationData modificationData,
                            Long userId) throws AccessDeniedException {
         if (!id.equals(userId)) {
@@ -63,12 +82,25 @@ public class UserService {
         return user;
     }
 
+    /**
+     * 유저식별자에 해당하는 유저를 삭제후 반환한다.
+     *
+     * @param id 유저 식별자
+     * @return 삭제될 유저
+     */
     public User deleteUser(Long id) {
         User user = findUser(id);
         user.destroy();
         return user;
     }
 
+    /**
+     * 유저 식별자에 해당하는 유저를 반환한다.
+     *
+     * @param id 유저식별자
+     * @return 식별자 해당 유저
+     * @throws UserNotFoundException 유저 식별자에 해당하는 유저를 찾을 수 없을 때
+ 때   */
     private User findUser(Long id) {
         return userRepository.findByIdAndDeletedIsFalse(id)
                 .orElseThrow(() -> new UserNotFoundException(id));
