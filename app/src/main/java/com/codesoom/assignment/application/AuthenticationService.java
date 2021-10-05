b241cb1e9b8a4ff28c768998a7aec25b38c6053f
pangnem <sprnd645@gmail.com>
2021-10-05 14:42:40 +0900

    docs: 누락된 Service Javadoc 추가

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -7,31 +7,42 @@ import com.codesoom.assignment.domain.UserRepository;
 import com.codesoom.assignment.errors.LoginFailException;
 import com.codesoom.assignment.utils.JwtUtil;
 import io.jsonwebtoken.Claims;
+import java.util.List;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
+/**
+ * 인증 관련 처리 담당.
+ */
 @Service
 public class AuthenticationService {
+
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final JwtUtil jwtUtil;
     private final PasswordEncoder passwordEncoder;
 
     public AuthenticationService(UserRepository userRepository,
-                                 RoleRepository roleRepository,
-                                 JwtUtil jwtUtil,
-                                 PasswordEncoder passwordEncoder) {
+        RoleRepository roleRepository,
+        JwtUtil jwtUtil,
+        PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.jwtUtil = jwtUtil;
         this.passwordEncoder = passwordEncoder;
     }
 
+    /**
+     * 회원 정보를 받아 액세스 토큰을 리턴합니다.
+     *
+     * @param email    회원의 이메일 주소
+     * @param password 회원의 비밀번호
+     * @return 액세스 토큰
+     * @throws LoginFailException 인증에 실패한 경우
+     */
     public String login(String email, String password) {
         User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new LoginFailException(email));
+            .orElseThrow(() -> new LoginFailException(email));
 
         if (!user.authenticate(password, passwordEncoder)) {
             throw new LoginFailException(email);
@@ -40,6 +51,12 @@ public class AuthenticationService {
         return jwtUtil.encode(user.getId());
     }
 
+    /**
+     * 액세스 토큰을 복호화 하여 회원의 id를 리턴합니다.
+     *
+     * @param accessToken 액세스 토큰
+     * @return 회원의 id
+     */
     public Long parseToken(String accessToken) {
         Claims claims = jwtUtil.decode(accessToken);
         return claims.get("userId", Long.class);
