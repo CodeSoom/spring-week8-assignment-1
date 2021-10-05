431cf38190405fdf87e6d0c43df43fa66b578a3b
pangnem <sprnd645@gmail.com>
2021-10-05 13:14:26 +0900

    docs: 누락된 controller Javadoc 추가

--- app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
@@ -4,21 +4,35 @@ import com.codesoom.assignment.application.AuthenticationService;
 import com.codesoom.assignment.dto.SessionRequestData;
 import com.codesoom.assignment.dto.SessionResponseData;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 세션 관련 HTTP 요청 처리 담당.
+ */
 @RestController
 @RequestMapping("/session")
 public class SessionController {
+
     private AuthenticationService authenticationService;
 
     public SessionController(AuthenticationService authenticationService) {
         this.authenticationService = authenticationService;
     }
 
+    /**
+     * 세션 요청 처리에 필요한 정보를 받고, 토큰을 포함한 응답을 리턴합니다.
+     *
+     * @param sessionRequestData 세션 요청 정보
+     * @return 세션 응답 정보
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public SessionResponseData login(
-            @RequestBody SessionRequestData sessionRequestData
+        @RequestBody SessionRequestData sessionRequestData
     ) {
         String email = sessionRequestData.getEmail();
         String password = sessionRequestData.getPassword();
@@ -26,7 +40,7 @@ public class SessionController {
         String accessToken = authenticationService.login(email, password);
 
         return SessionResponseData.builder()
-                .accessToken(accessToken)
-                .build();
+            .accessToken(accessToken)
+            .build();
     }
 }
--- app/src/main/java/com/codesoom/assignment/controllers/UserController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/UserController.java
@@ -6,22 +6,38 @@ import com.codesoom.assignment.dto.UserModificationData;
 import com.codesoom.assignment.dto.UserRegistrationData;
 import com.codesoom.assignment.dto.UserResultData;
 import com.codesoom.assignment.security.UserAuthentication;
+import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-
+/**
+ * 회원 관련 HTTP 요청 처리 담당.
+ */
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
     private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
+    /**
+     * 회원 생성 데이터로 회원을 생성하고 리턴합니다.
+     *
+     * @param registrationData 회원 생성 데이터
+     * @return 생성된 회원
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     UserResultData create(@RequestBody @Valid UserRegistrationData registrationData) {
@@ -29,18 +45,36 @@ public class UserController {
         return getUserResultData(user);
     }
 
+    /**
+     * 식별자로 회원 정보를 찾아 수정하고, 리턴합니다.
+     * <p>
+     * 유저 권한을 가진 인증된 유저만 이 요청을 수행할 수 있습니다.
+     *
+     * @param id               식별자
+     * @param modificationData 수정할 회원 정보
+     * @param authentication   회원 인증 정보
+     * @return 수정된 회원 정보
+     * @throws AccessDeniedException 다른 사람의 정보를 수정하려는 경우
+     */
     @PatchMapping("{id}")
     @PreAuthorize("isAuthenticated() and hasAuthority('USER')")
     UserResultData update(
-            @PathVariable Long id,
-            @RequestBody @Valid UserModificationData modificationData,
-            UserAuthentication authentication
+        @PathVariable Long id,
+        @RequestBody @Valid UserModificationData modificationData,
+        UserAuthentication authentication
     ) throws AccessDeniedException {
         Long userId = authentication.getUserId();
         User user = userService.updateUser(id, modificationData, userId);
         return getUserResultData(user);
     }
 
+    /**
+     * 식별자로 회원을 찾아 삭제합니다.
+     * <p>
+     * 어드민 권한을 가진 인증된 유저만 이 요청을 수행할 수 있습니다.
+     *
+     * @param id 식별자
+     */
     @DeleteMapping("{id}")
     @PreAuthorize("isAuthenticated() and hasAuthority('ADMIN')")
     void destroy(@PathVariable Long id) {
@@ -53,9 +87,9 @@ public class UserController {
         }
 
         return UserResultData.builder()
-                .id(user.getId())
-                .email(user.getEmail())
-                .name(user.getName())
-                .build();
+            .id(user.getId())
+            .email(user.getEmail())
+            .name(user.getName())
+            .build();
     }
 }
