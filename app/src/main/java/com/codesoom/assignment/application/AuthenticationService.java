38b3fe199e111ee3899f82248332f0e765c3e9e1
catsbi <killfine1988@gmail.com>
2021-10-05 22:49:35 +0900

    feat: Service Layer 주석 작성

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -12,6 +12,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * 인증 서비스를 제공한다.
+ */
 @Service
 public class AuthenticationService {
     private final UserRepository userRepository;
@@ -29,6 +32,14 @@ public class AuthenticationService {
         this.passwordEncoder = passwordEncoder;
     }
 
+    /**
+     * 로그인 정보를 확인 후 인증 토큰을 반환한다.
+     *
+     * @param email 이메일 정보
+     * @param password 비밀 번호
+     * @return 인증 토큰
+     * @throws LoginFailException 이메일로 회원을 찾을 수 없거나 비밀번호가 일치하지 않을 경우 예외를 던진다.
+     */
     public String login(String email, String password) {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new LoginFailException(email));
@@ -40,11 +51,23 @@ public class AuthenticationService {
         return jwtUtil.encode(user.getId());
     }
 
+    /**
+     * 인증 토큰을 분석해 식별자 정보를 반환한다.
+     *
+     * @param accessToken 인증 토큰
+     * @return 식별자 정보
+     */
     public Long parseToken(String accessToken) {
         Claims claims = jwtUtil.decode(accessToken);
         return claims.get("userId", Long.class);
     }
 
+    /**
+     * 해당 식별자의 권한을 확인한다.
+     *
+     * @param userId 식별자
+     * @return 권한 목록
+     */
     public List<Role> roles(Long userId) {
         return roleRepository.findAllByUserId(userId);
     }
