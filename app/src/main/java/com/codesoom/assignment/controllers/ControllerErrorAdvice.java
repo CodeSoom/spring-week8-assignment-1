c4684dd3dbe0b9dfc4d3aaa843320bc1652ac77a
HyoungUkJJang <guddnr1451@gmail.com>
2021-10-06 16:14:55 +0900

    ADD 예외 처리 관련 자바독 작성

--- app/src/main/java/com/codesoom/assignment/controllers/ControllerErrorAdvice.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ControllerErrorAdvice.java
@@ -11,27 +11,47 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * HTTP 요청에 따른 예외처리 기능을 제공하는 클래스
+ */
 @ResponseBody
 @ControllerAdvice
 public class ControllerErrorAdvice {
+
+    /**
+     * 상품을 찾지 못할경우 발생시킬 예외
+     * @return 상품을 찾지 못하는 응답 메시지
+     */
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ProductNotFoundException.class)
     public ErrorResponse handleProductNotFound() {
         return new ErrorResponse("Product not found");
     }
 
+    /**
+     * 사용자를 찾지 못할경우 발생시킬 예외
+     * @return 사용자를 찾지 못하는 응답 메시지
+     */
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(UserNotFoundException.class)
     public ErrorResponse handleUserNotFound() {
         return new ErrorResponse("User not found");
     }
 
+    /**
+     * 사용자 이메일이 중복일 경우 발생시킬 예외
+     * @return 입력한 이메일이 이미 존재한다는 응답 메시지
+     */
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(UserEmailDuplicationException.class)
     public ErrorResponse handleUserEmailIsAlreadyExisted() {
         return new ErrorResponse("User's email address is already existed");
     }
 
+    /**
+     * 로그인에 실패할 경우 발생시킬 예외
+     * @return 로그인 실패 응답 메시지
+     */
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(LoginFailException.class)
     public ErrorResponse handleLoginFailException() {
