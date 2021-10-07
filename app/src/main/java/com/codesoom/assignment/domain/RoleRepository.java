2913862e86daf3f35c89be1ce131524d6139ce66
daeungkim <daeungkim93@gmail.com>
2021-10-08 05:46:54 +0900

    오타 수정
    오타 수정

--- app/src/main/java/com/codesoom/assignment/domain/RoleRepository.java
+++ app/src/main/java/com/codesoom/assignment/domain/RoleRepository.java
@@ -17,7 +17,7 @@ public interface RoleRepository {
     /**
      * 사용자 권한을 저장한다.
      *
-     * @param role 저장할 사용자 권한ㄷ
+     * @param role 저장할 사용자 권한
      * @return 저장한 사용자 권한
      */
     Role save(Role role);
