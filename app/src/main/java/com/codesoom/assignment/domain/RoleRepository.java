c0a0e0c3d11c080f6fe8ec94252523d906ce3665
daeungkim <daeungkim93@gmail.com>
2021-10-07 20:22:43 +0900

    Role RoleRepository javadoc 작성
    Role RoleRepository javadoc 작성

--- app/src/main/java/com/codesoom/assignment/domain/RoleRepository.java
+++ app/src/main/java/com/codesoom/assignment/domain/RoleRepository.java
@@ -2,8 +2,23 @@ package com.codesoom.assignment.domain;
 
 import java.util.List;
 
+/**
+ * 사용자 권한 생성 및 조회를 담당한다.
+ */
 public interface RoleRepository {
+    /**
+     * 사용자 권한 목록을 조회한다.
+     *
+     * @param userId 조회할 사용자의 id
+     * @return 사용자 권한 목록
+     */
     List<Role> findAllByUserId(Long userId);
 
+    /**
+     * 사용자 권한을 저장한다.
+     *
+     * @param role 저장할 사용자 권한ㄷ
+     * @return 저장한 사용자 권한
+     */
     Role save(Role role);
 }
