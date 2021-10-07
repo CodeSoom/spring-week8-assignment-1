5bb87f064baba5b13480a6cc7604ea370705b136
daeungkim <daeungkim93@gmail.com>
2021-10-07 20:13:07 +0900

    AuthenticationService javadoc 작성
    AuthenticationService javadoc 작성

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -4,6 +4,7 @@ import com.codesoom.assignment.domain.Role;
 import com.codesoom.assignment.domain.RoleRepository;
 import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.domain.UserRepository;
+import com.codesoom.assignment.errors.InvalidTokenException;
 import com.codesoom.assignment.errors.LoginFailException;
 import com.codesoom.assignment.utils.JwtUtil;
 import io.jsonwebtoken.Claims;
@@ -12,6 +13,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * 인증 토큰 생성, 검증 및 사용자의 권한 조회를 담당한다.
+ */
 @Service
 public class AuthenticationService {
     private final UserRepository userRepository;
@@ -29,6 +33,14 @@ public class AuthenticationService {
         this.passwordEncoder = passwordEncoder;
     }
 
+    /**
+     * 인증 토큰을 리턴한다.
+     *
+     * @param email 이메일
+     * @param password 비밀번호
+     * @return 인증 토큰
+     * @throws LoginFailException 로그인에 사용된 데이터가 잘못된 경우
+     */
     public String login(String email, String password) {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new LoginFailException(email));
@@ -40,11 +52,24 @@ public class AuthenticationService {
         return jwtUtil.encode(user.getId());
     }
 
+    /**
+     * 인증 토큰을 검증한다.
+     *
+     * @param accessToken 검증할 인증 토큰
+     * @return 사용자 id
+     * @throws InvalidTokenException 인증 토큰이 유효하지 않은 경우
+     */
     public Long parseToken(String accessToken) {
         Claims claims = jwtUtil.decode(accessToken);
         return claims.get("userId", Long.class);
     }
 
+    /**
+     * 사용자의 권한 목록을 리턴한다.
+     *
+     * @param userId 조회할 사용자의 id
+     * @return 사용자의 권한 목록
+     */
     public List<Role> roles(Long userId) {
         return roleRepository.findAllByUserId(userId);
     }
