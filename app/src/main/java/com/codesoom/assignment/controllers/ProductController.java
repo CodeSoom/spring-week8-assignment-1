07fed7f64375dbcea160fe2624a2cb13aec5315f
daeungkim <daeungkim93@gmail.com>
2021-10-07 19:34:44 +0900

    UserController javadoc 작성
    UserController javadoc 작성

--- app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
@@ -7,7 +7,15 @@ import com.codesoom.assignment.dto.ProductData;
 import com.codesoom.assignment.errors.ProductNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -53,7 +61,7 @@ public class ProductController {
     /**
      * 생성한 상품을 리턴한다.
      *
-     * @param productData 생성할 데이터
+     * @param productData 상품 생성에 필요한 데이터
      * @return 생성한 상품
      */
     @PostMapping
--- app/src/main/java/com/codesoom/assignment/controllers/UserController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/UserController.java
@@ -6,13 +6,24 @@ import com.codesoom.assignment.dto.UserModificationData;
 import com.codesoom.assignment.dto.UserRegistrationData;
 import com.codesoom.assignment.dto.UserResultData;
 import com.codesoom.assignment.security.UserAuthentication;
+import com.codesoom.assignment.errors.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+/**
+ * 사용자의 생성, 조회, 수정, 삭제를 담당한다.
+ */
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -22,6 +33,12 @@ public class UserController {
         this.userService = userService;
     }
 
+    /**
+     * 생성한 사용자 정보를 리턴한다.
+     *
+     * @param registrationData 사용자 생성에 필요한 데이터
+     * @return 생성한 사용자의 데이터
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     UserResultData create(@RequestBody @Valid UserRegistrationData registrationData) {
@@ -29,6 +46,16 @@ public class UserController {
         return getUserResultData(user);
     }
 
+    /**
+     * 수정한 사용자 정보를 리턴한다.
+     *
+     * @param id 수정할 사용자의 id
+     * @param modificationData 수정할 사용자 데이터
+     * @param authentication 수정을 요청한 사용자의 인증정보
+     * @return 수정한 사용자의 데이터
+     * @throws UserNotFoundException 수정할 사용자를 찾지 못한 경우
+     * @throws AccessDeniedException 수정할 사용자와 수정을 요청한 사용자가 일치하지 않는 경우
+     */
     @PatchMapping("{id}")
     @PreAuthorize("isAuthenticated() and hasAuthority('USER')")
     UserResultData update(
@@ -41,6 +68,11 @@ public class UserController {
         return getUserResultData(user);
     }
 
+    /**
+     * 사용자를 삭제한다.
+     * @param id 삭제할 사용자의 id
+     * @throws UserNotFoundException 삭제할 사용자를 찾지 못한 경우
+     */
     @DeleteMapping("{id}")
     @PreAuthorize("isAuthenticated() and hasAuthority('ADMIN')")
     void destroy(@PathVariable Long id) {
