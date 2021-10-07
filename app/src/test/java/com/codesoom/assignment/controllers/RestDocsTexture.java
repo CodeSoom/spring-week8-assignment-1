608f930dfe56423221dd2bad6318f1090c883e91
Sungeheon <elon4856@gmail.com>
2021-10-07 20:12:21 +0900

    fix: parameter표에 알맞게 설명을 축약하세요.

--- app/src/test/java/com/codesoom/assignment/controllers/RestDocsTexture.java
+++ app/src/test/java/com/codesoom/assignment/controllers/RestDocsTexture.java
@@ -21,13 +21,12 @@ public class RestDocsTexture {
     static RequestFieldsSnippet getProductRequestFieldsSnippet() {
         return requestFields(
                 fieldWithPath("name").type(STRING).description("상품 이름")
-                        .attributes(key("constraints").value("한 글자 이상 입력해야합니다.")),
+                        .attributes(key("constraints").value("한 글자 이상")),
                 fieldWithPath("maker").type(STRING).description("상품 제조사")
-                        .attributes(key("constraints").value("한 글자 이상 입력해야합니다.")),
+                        .attributes(key("constraints").value("한 글자 이상")),
                 fieldWithPath("price").type(NUMBER).description("상품 가격")
-                        .attributes(key("constraints").value("빈 값을 입력할 수 없습니다.")),
+                        .attributes(key("constraints").value("빈 값을 입력 불가")),
                 fieldWithPath("imageUrl").type(STRING).description("상품 이미지").optional()
-                        .attributes(key("constraints").value(""))
         );
     }
     static ResponseFieldsSnippet getProductResponseFieldsSnippet() {
@@ -53,19 +52,19 @@ public class RestDocsTexture {
     static RequestFieldsSnippet getUserPostRequestFieldsSnippet() {
         return requestFields(
                 fieldWithPath("email").type(STRING).description("이메일")
-                        .attributes(key("constraints").value("3 글자 이상 입력해야 한다.")),
+                        .attributes(key("constraints").value("3 글자 이상")),
                 fieldWithPath("name").type(STRING).description("이름")
-                        .attributes(key("constraints").value("1 글자 이상 입력해야 한다.")),
+                        .attributes(key("constraints").value("1 글자 이상")),
                 fieldWithPath("password").type(STRING).description("비밀번호")
-                        .attributes(key("constraints").value("4 글자 이상 입력해야 한다.")));
+                        .attributes(key("constraints").value("4 글자 이상")));
     }
 
     static RequestFieldsSnippet getUserPatchRequestFieldsSnippet() {
         return requestFields(
                 fieldWithPath("name").type(STRING).description("이름")
-                        .attributes(key("constraints").value("1 글자 이상 입력해야 한다.")),
+                        .attributes(key("constraints").value("1 글자 이상")),
                 fieldWithPath("password").type(STRING).description("비밀번호")
-                        .attributes(key("constraints").value("4 글자 이상 입력해야 한다.")));
+                        .attributes(key("constraints").value("4 글자 이상")));
     }
 
     static ResponseFieldsSnippet getUserResponseFieldsSnippet() {
