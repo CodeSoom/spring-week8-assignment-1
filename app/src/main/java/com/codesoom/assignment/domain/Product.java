057feb8da956620083c3fdb24295c198397f024a
daeungkim <daeungkim93@gmail.com>
2021-10-07 20:17:29 +0900

    Product ProductRepository javadoc 작성
    Product ProductRepository javadoc 작성

--- app/src/main/java/com/codesoom/assignment/domain/Product.java
+++ app/src/main/java/com/codesoom/assignment/domain/Product.java
@@ -9,6 +9,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+/**
+ * 상품 도메인
+ */
 @Entity
 @Getter
 @Builder
@@ -27,6 +30,11 @@ public class Product {
 
     private String imageUrl;
 
+    /**
+     * 상품이 가지고 있는 데이터를 수정한다.
+     *
+     * @param source 수정할 데이터
+     */
     public void changeWith(Product source) {
         this.name = source.name;
         this.maker = source.maker;
--- app/src/main/java/com/codesoom/assignment/domain/ProductRepository.java
+++ app/src/main/java/com/codesoom/assignment/domain/ProductRepository.java
@@ -3,12 +3,37 @@ package com.codesoom.assignment.domain;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * 상품 조회, 저장, 삭제를 담당한다.
+ */
 public interface ProductRepository {
+    /**
+     * 상품의 목록을 리턴한다.
+     *
+     * @return 상품의 목록
+     */
     List<Product> findAll();
 
+    /**
+     * id에 해당하는 상품을 리턴한다.
+     *
+     * @param id 조회할 상품의 id
+     * @return 조회 결과
+     */
     Optional<Product> findById(Long id);
 
+    /**
+     * 상품을 저장한다.
+     *
+     * @param product 저장할 상품
+     * @return 저장한 상품
+     */
     Product save(Product product);
 
+    /**
+     * 상품을 삭제한다.
+     *
+     * @param product 삭제할 상품
+     */
     void delete(Product product);
 }
