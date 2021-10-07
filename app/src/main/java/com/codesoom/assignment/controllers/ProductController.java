7abae7b5ee9de03157154a59a8408d18585b68b0
eastshine-high <eastshine.high@gmail.com>
2021-10-07 19:42:04 +0900

    ProductController에 대한 javadoc 추가

--- app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+/**
+ * products에 대한 HTTP 요청을 처리한다.
+ */
 @RestController
 @RequestMapping("/products")
 public class ProductController {
@@ -24,23 +27,48 @@ public class ProductController {
         this.authenticationService = authenticationService;
     }
 
+    /**
+     * products의 get 요청을 처리하고 List<Product>를 응답
+     *
+     * @return List<Product> 상품 목록
+     */
     @GetMapping
     public List<Product> list() {
         return productService.getProducts();
     }
 
+    /**
+     * products/{id}에 대한 get 요청을 처리하고 id에 해당하는 Product를 응답
+     *
+     * @param id 상품의 식별자
+     * @return 식별자에 해댱하는 상품
+     */
     @GetMapping("{id}")
     public Product detail(@PathVariable Long id) {
         return productService.getProduct(id);
     }
 
+    /**
+     * products에 대한 post 요청을 처리하고 등록된 Product 응답
+     *
+     * @param productData 등록할 상품 정보
+     * @return 등록된 상품 정보
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("isAuthenticated() and hasAuthority('USER')")
     public Product create(@RequestBody @Valid ProductData productData) {
+        // location reponse header
         return productService.createProduct(productData);
     }
 
+    /**
+     * products/{id}에 대한 patch 요청을 처리하고 수정한 Product를 응답
+     *
+     * @param id Product의 식별자
+     * @param productData 수정할 Product의 목표 정보
+     * @return 수정된 Product
+     */
     @PatchMapping("{id}")
     @PreAuthorize("isAuthenticated()")
     public Product update(
@@ -50,6 +78,11 @@ public class ProductController {
         return productService.updateProduct(id, productData);
     }
 
+    /**
+     * products/{id}에 대한 delete 요청을 처리
+     *
+     * @param id Product 식별자
+     */
     @DeleteMapping("{id}")
     @PreAuthorize("isAuthenticated()")
     public void destroy(@PathVariable Long id) {
