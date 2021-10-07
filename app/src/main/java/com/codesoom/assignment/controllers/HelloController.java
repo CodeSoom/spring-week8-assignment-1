81972b6a963474481fa71d57d523342872b7c930
daeungkim <daeungkim93@gmail.com>
2021-10-07 19:19:01 +0900

    HelloController javadoc 작성
    HelloController javadoc 작성

--- app/src/main/java/com/codesoom/assignment/controllers/HelloController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/HelloController.java
@@ -3,6 +3,9 @@ package com.codesoom.assignment.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * api 서버의 정상 작동여부를 확인 요청을 담당한다.
+ */
 @RestController
 public class HelloController {
     @RequestMapping("/")
