09efada4b652a71c75a5a4af0e9bc77864c61e68
HyoungUkJJang <guddnr1451@gmail.com>
2021-10-07 16:12:06 +0900

    ADD 10/7 시큐리티 관련 클래스 자바독 작성

--- app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
+++ app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
@@ -14,12 +14,20 @@ import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 import javax.servlet.Filter;
 
+/**
+ * 웹 Security 관련 설정 클래스
+ */
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private AuthenticationService authenticationService;
 
+    /**
+     * HTTP 요청에 관한 보안들을 설정한다.
+     * @param http 보안 정보
+     * @throws Exception 보안 설정이 실패한 경우
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         Filter authenticationFilter = new JwtAuthenticationFilter(
