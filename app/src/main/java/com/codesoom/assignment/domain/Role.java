f892742ccb3571e2feb807a3653a333278211259
HyoungUkJJang <guddnr1451@gmail.com>
2021-10-07 16:22:45 +0900

    ADD 권한 관련 클래스 자바독 작성

--- app/src/main/java/com/codesoom/assignment/domain/Role.java
+++ app/src/main/java/com/codesoom/assignment/domain/Role.java
@@ -7,15 +7,25 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+/**
+ * 사용자의 권한 정보를 가지고있는 도메인
+ */
 @Entity
 @NoArgsConstructor
 public class Role {
+
     @Id
     @GeneratedValue
     private Long id;
 
+    /**
+     * 사용자의 식별자
+     */
     private Long userId;
 
+    /**
+     * 사용자의 권한 이름
+     */
     @Getter
     private String name;
 
