3c938cc576eede3b6d68bc95e439ceeff515a006
kyuwon53 <lkw2v@naver.com>
2022-01-17 20:47:38 +0900

    [Fix] javadoc을 수정하라 . 빠진 부분 붙이고 id에 대한 설명 통일

--- app/src/main/java/com/codesoom/assignment/application/ProductService.java
+++ app/src/main/java/com/codesoom/assignment/application/ProductService.java
@@ -29,7 +29,7 @@ public class ProductService {
 
     /**
      * 이 어플리케이션에 있는 모든 상품목록을 리턴합니다.
-     * @return 모든 상품들
+     * @return 모든 상품들.
      */
     public List<Product> getProducts() {
         return productRepository.findAll();
@@ -37,7 +37,7 @@ public class ProductService {
 
     /**
      * 주어진 id에 해당하는 상품을 리턴합니다.
-     * @param id 는 상품을 식별하는 고유 값.
+     * @param id 상품을 식별하는 고유 값.
      * @return id에 해당하는 상품.
      * @throws ProductNotFoundException 상품을 찾을 수 없을 경우 발생.
      */
@@ -47,8 +47,8 @@ public class ProductService {
 
     /**
      * 주어진 상품 정보를 저장하고 리턴합니다.
-     * @param productData 주어진 상품 정보
-     * @return 상품 정보
+     * @param productData 주어진 상품 정보.
+     * @return 상품 정보.
      */
     public Product createProduct(ProductData productData) {
         Product product = mapper.map(productData, Product.class);
@@ -57,7 +57,7 @@ public class ProductService {
 
     /**
      * 주어진 id에 해당하는 상품의 정보를 수정하고 수정된 정보를 리턴합니다.
-     * @param id 상품 식별자.
+     * @param id 상품을 식별하는 고유 값.
      * @param productData 수정된 상품 정보.
      * @return 수정된 상품.
      * @throws ProductNotFoundException 상품을 찾을 수 없을 경우 발생.
@@ -72,7 +72,7 @@ public class ProductService {
 
     /**
      * 주어진 id에 해당하는 상품을 삭제하고 리턴합니다.
-     * @param id 상품 식별자.
+     * @param id 상품을 식별하는 고유 값.
      * @return 삭제된 상품.
      * @throws ProductNotFoundException 상품을 찾을 수 없을 경우 발생.
      */
@@ -87,8 +87,8 @@ public class ProductService {
     /**
      * 주어진 id에 해당하는 상품을 찾고 있다면 상품을 리턴하고
      * 없다면 상품을 찾을 수 없다는 예외를 던진다.
-     * @param id 상품 식별자.
-     * @return 해당 상품
+     * @param id 상품을 식별하는 고유 값.
+     * @return 해당 상품.
      * @throws ProductNotFoundException 상품을 찾을 수 없을 경우 발생.
      */
     private Product findProduct(Long id) {
