b42c71166965a1c4bef816585587f5304365d408
kyuwon53 <lkw2v@naver.com>
2022-01-17 21:26:20 +0900

    [Fix] javadoc 표현을 변경하라 상품목록에 관한 javadoc 수정 마침표 수정

--- app/src/main/java/com/codesoom/assignment/application/ProductService.java
+++ app/src/main/java/com/codesoom/assignment/application/ProductService.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 /**
  * 상품을 관리한다.
+ * @author kyuwon aka (kyuwoon369@gmail.com)
  */
 @Service
 @Transactional
@@ -28,8 +29,8 @@ public class ProductService {
     }
 
     /**
-     * 이 어플리케이션에 있는 모든 상품목록을 리턴합니다.
-     * @return 모든 상품들.
+     * 저장된 모든 상품목록을 리턴합니다.
+     * @return 모든 상품들
      */
     public List<Product> getProducts() {
         return productRepository.findAll();
@@ -37,9 +38,9 @@ public class ProductService {
 
     /**
      * 주어진 id에 해당하는 상품을 리턴합니다.
-     * @param id 상품을 식별하는 고유 값.
-     * @return id에 해당하는 상품.
-     * @throws ProductNotFoundException 상품을 찾을 수 없을 경우 발생.
+     * @param id 상품을 식별하는 고유 값
+     * @return id에 해당하는 상품
+     * @throws ProductNotFoundException 상품을 찾을 수 없을 경우.
      */
     public Product getProduct(Long id) {
         return findProduct(id);
@@ -47,8 +48,8 @@ public class ProductService {
 
     /**
      * 주어진 상품 정보를 저장하고 리턴합니다.
-     * @param productData 주어진 상품 정보.
-     * @return 상품 정보.
+     * @param productData 주어진 상품 정보
+     * @return 상품 정보
      */
     public Product createProduct(ProductData productData) {
         Product product = mapper.map(productData, Product.class);
@@ -57,10 +58,10 @@ public class ProductService {
 
     /**
      * 주어진 id에 해당하는 상품의 정보를 수정하고 수정된 정보를 리턴합니다.
-     * @param id 상품을 식별하는 고유 값.
-     * @param productData 수정된 상품 정보.
-     * @return 수정된 상품.
-     * @throws ProductNotFoundException 상품을 찾을 수 없을 경우 발생.
+     * @param id 상품을 식별하는 고유 값
+     * @param productData 수정된 상품 정보
+     * @return 수정된 상품
+     * @throws ProductNotFoundException 상품을 찾을 수 없을 경우.
      */
     public Product updateProduct(Long id, ProductData productData) {
         Product product = findProduct(id);
@@ -72,9 +73,9 @@ public class ProductService {
 
     /**
      * 주어진 id에 해당하는 상품을 삭제하고 리턴합니다.
-     * @param id 상품을 식별하는 고유 값.
-     * @return 삭제된 상품.
-     * @throws ProductNotFoundException 상품을 찾을 수 없을 경우 발생.
+     * @param id 상품을 식별하는 고유 값
+     * @return 삭제된 상품
+     * @throws ProductNotFoundException 상품을 찾을 수 없을 경우.
      */
     public Product deleteProduct(Long id) {
         Product product = findProduct(id);
@@ -87,8 +88,8 @@ public class ProductService {
     /**
      * 주어진 id에 해당하는 상품을 찾고 있다면 상품을 리턴하고
      * 없다면 상품을 찾을 수 없다는 예외를 던진다.
-     * @param id 상품을 식별하는 고유 값.
-     * @return 해당 상품.
+     * @param id 상품을 식별하는 고유 값
+     * @return 해당 상품
      * @throws ProductNotFoundException 상품을 찾을 수 없을 경우 발생.
      */
     private Product findProduct(Long id) {
