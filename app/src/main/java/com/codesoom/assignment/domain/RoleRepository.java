b46f7d07385c31bccca3b9badb416727e4996497
kyuwon53 <lkw2v@naver.com>
2022-01-19 20:06:41 +0900

    [Add] 사용자 권한 레포지토리에 javadoc을 추가하라

--- app/src/main/java/com/codesoom/assignment/domain/RoleRepository.java
+++ app/src/main/java/com/codesoom/assignment/domain/RoleRepository.java
@@ -2,8 +2,23 @@ package com.codesoom.assignment.domain;
 
 import java.util.List;
 
+/**
+ * 사용자 권한을 저장, 찾는 클래스
+ */
 public interface RoleRepository {
+    /**
+     * 주어진 id에 해당하는 권한 목록을 반환한다.
+     *
+     * @param userId
+     * @return 권한 목록
+     */
     List<Role> findAllByUserId(Long userId);
 
+    /**
+     * 사용자 권한을 저장하고 반환한다.
+     *
+     * @param role 권한 정보
+     * @return 저장한 권한
+     */
     Role save(Role role);
 }
