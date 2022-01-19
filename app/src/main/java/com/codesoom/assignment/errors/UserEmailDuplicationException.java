1f936bf7371e4959a4ae732223c5e2aa3fa24dc5
kyuwon53 <lkw2v@naver.com>
2022-01-19 20:28:27 +0900

    [Add] 사용자 이메일이 중복일 경우 발생되는 예외 javadoc을 작성

--- app/src/main/java/com/codesoom/assignment/errors/UserEmailDuplicationException.java
+++ app/src/main/java/com/codesoom/assignment/errors/UserEmailDuplicationException.java
@@ -1,5 +1,8 @@
 package com.codesoom.assignment.errors;
 
+/**
+ * 사용자 이메일이 중복일 경우 발생되는 예외 클래스
+ */
 public class UserEmailDuplicationException extends RuntimeException {
     public UserEmailDuplicationException(String email) {
         super("User email is alreay existed: " + email);
