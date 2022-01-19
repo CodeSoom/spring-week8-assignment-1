c64233ca3844a8057715560241ee283ff868e932
kyuwon53 <lkw2v@naver.com>
2022-01-19 20:31:35 +0900

    [Add] 사용자를 찾지 못할 경우 예외 클래스에 javadoc을 작성

--- app/src/main/java/com/codesoom/assignment/errors/UserNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/errors/UserNotFoundException.java
@@ -1,5 +1,8 @@
 package com.codesoom.assignment.errors;
 
+/**
+ * 사용자를 찾지 못할 경우 발생되는 예외 클래스
+ */
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(Long id) {
         super("User not found: " + id);
