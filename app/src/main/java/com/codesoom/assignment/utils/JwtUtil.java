12490c6b93ab90058146942da184842cfefb39e3
kyuwon53 <lkw2v@naver.com>
2022-01-19 20:47:21 +0900

    [Add] JWT 클래스에 javadoc을 작성하라

--- app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
+++ app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
@@ -10,6 +10,9 @@ import org.springframework.stereotype.Component;
 
 import java.security.Key;
 
+/**
+ * JWT 토큰을 생성한다.
+ */
 @Component
 public class JwtUtil {
     private final Key key;
@@ -18,6 +21,12 @@ public class JwtUtil {
         key = Keys.hmacShaKeyFor(secret.getBytes());
     }
 
+    /**
+     * id를 암호화하여 토큰을 생성 후 리턴한다.
+     *
+     * @param userId 사용자 id
+     * @return 생성된 토큰
+     */
     public String encode(Long userId) {
         return Jwts.builder()
                 .claim("userId", userId)
@@ -25,6 +34,13 @@ public class JwtUtil {
                 .compact();
     }
 
+    /**
+     * 토큰을 복호화하여 리턴한다.
+     *
+     * @param token 사용자의 토큰
+     * @return 복호화된 토큰
+     * @throws InvalidTokenException 토큰값이 비어있거나 유효하지 않은 경우
+     */
     public Claims decode(String token) {
         if (token == null || token.isBlank()) {
             throw new InvalidTokenException(token);
