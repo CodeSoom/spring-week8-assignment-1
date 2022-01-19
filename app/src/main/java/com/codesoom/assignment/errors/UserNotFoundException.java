1a47508156ef8d8af071867d5796e5175b98f27b
kyuwon53 <lkw2v@naver.com>
2022-01-19 22:14:20 +0900

    [Fix] 사용자를 찾지 못한 경우 javadoc을 수정하라

--- app/src/main/java/com/codesoom/assignment/errors/UserNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/errors/UserNotFoundException.java
@@ -1,7 +1,7 @@
 package com.codesoom.assignment.errors;
 
 /**
- * 사용자를 찾지 못할 경우 발생되는 예외 클래스
+ * 사용자를 찾지 못할 경우 던집니다.
  */
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(Long id) {
