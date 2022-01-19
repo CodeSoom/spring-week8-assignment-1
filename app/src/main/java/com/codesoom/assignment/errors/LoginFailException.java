ded4374e830d543aa9d1a122dd2e7ee6fb665e50
kyuwon53 <lkw2v@naver.com>
2022-01-19 20:26:41 +0900

    [Add] 사용자 로그인 실패 예외 클래스에 javadoc을 작성하라

--- app/src/main/java/com/codesoom/assignment/errors/LoginFailException.java
+++ app/src/main/java/com/codesoom/assignment/errors/LoginFailException.java
@@ -1,5 +1,8 @@
 package com.codesoom.assignment.errors;
 
+/**
+ * 사용자가 로그인에 실패할 경우 발생되는 예외 클래스
+ */
 public class LoginFailException extends RuntimeException {
     public LoginFailException(String email) {
         super("Login fail - email: " + email);
