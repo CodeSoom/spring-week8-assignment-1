659207ed3cf1877bfbc2748887a68d7150eff8c2
kyuwon53 <lkw2v@naver.com>
2022-01-19 22:06:26 +0900

    [Fix] javadoc을 수정하라

--- app/src/main/java/com/codesoom/assignment/errors/InvalidTokenException.java
+++ app/src/main/java/com/codesoom/assignment/errors/InvalidTokenException.java
@@ -1,7 +1,7 @@
 package com.codesoom.assignment.errors;
 
 /**
- * 사용자 토큰이 유효하지 않을 경우 발생시킬 예외
+ * 사용자 토큰이 유효하지 않을 경우 던집니다.
  */
 public class InvalidTokenException extends RuntimeException {
     public InvalidTokenException(String token) {
