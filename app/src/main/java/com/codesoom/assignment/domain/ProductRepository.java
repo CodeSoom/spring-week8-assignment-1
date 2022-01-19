6f8dec44cdc0f29e54c66456f1efc876d27c58a5
kyuwon53 <lkw2v@naver.com>
2022-01-19 20:04:44 +0900

    [Add] 상품 레포지토리에 javadoc 추가

--- app/src/main/java/com/codesoom/assignment/domain/ProductRepository.java
+++ app/src/main/java/com/codesoom/assignment/domain/ProductRepository.java
@@ -3,6 +3,9 @@ package com.codesoom.assignment.domain;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * 상품을 저장소에 찾기,저장,삭제 기능을 제공한다.
+ */
 public interface ProductRepository {
     List<Product> findAll();
 
