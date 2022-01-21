1a48313b603ddcba1e4b74c26d297a0a1e0aef42
hyuk <pjh08190819@naver.com>
2022-01-22 01:24:45 +0900

    docs : SessionController 의 javadoc 작성

--- app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/SessionController.java
@@ -6,6 +6,11 @@ import com.codesoom.assignment.dto.SessionResponseData;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * 인증에 대한 요청 처리
+ *
+ * @author 혁 (pjh08190819@gmail.com)
+ */
 @RestController
 @RequestMapping("/session")
 public class SessionController {
@@ -15,6 +20,12 @@ public class SessionController {
         this.authenticationService = authenticationService;
     }
 
+    /**
+     * 로그인 체크를 하고 token을 생성하여 반환합니다.
+     *
+     * @param sessionRequestData
+     * @return
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public SessionResponseData login(
