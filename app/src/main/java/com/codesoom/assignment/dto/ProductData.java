85ed994895f84fb600ec52b561527db0700db5ab
shineoov <shineoov88@gmail.com>
2022-05-11 16:01:01 +0900

    ProductData Javadoc 작성

--- app/src/main/java/com/codesoom/assignment/dto/ProductData.java
+++ app/src/main/java/com/codesoom/assignment/dto/ProductData.java
@@ -10,6 +10,9 @@ import lombok.Setter;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+/**
+ * 상품 등록 또는 수정에 필요한 데이터를 정의합니다.
+ */
 @Setter
 @Getter
 @Builder
