eb834f403472d984c9c87c4030c160437320a189
이병성 <lbs243120@gmail.com>
2022-05-11 14:53:05 +0900

    java doc write

--- app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/ProductController.java
@@ -2,6 +2,7 @@ package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.application.AuthenticationService;
 import com.codesoom.assignment.application.ProductService;
+import com.codesoom.assignment.common.CommonResponse;
 import com.codesoom.assignment.domain.Product;
 import com.codesoom.assignment.dto.ProductData;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+/**
+ * 클라이언트로 부터 crud 요청과 요청에 대한 응답을 처리하는 controller입니다
+ */
 @RestController
 @RequestMapping("/products")
 public class ProductController {
@@ -24,16 +28,37 @@ public class ProductController {
         this.authenticationService = authenticationService;
     }
 
+    /**
+     * 등록된 모든 상품 목록과 상태코드 200을 응답합니다
+     *
+     * @return 모든 상품 목록
+     */
     @GetMapping
-    public List<Product> list() {
-        return productService.getProducts();
+    public CommonResponse<List<Product>> list() {
+        List<Product> list = productService.getProducts();
+        return CommonResponse.success(list);
     }
 
+    /**
+     * 등록된 상품중 id에 해당하는 상품 상세 정보와 상태코드 200을 응답합니다.
+     *
+     * @param id 상품의 id
+     * @return id에 해당하는 상품의 상세 정보
+     */
     @GetMapping("{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Product detail(@PathVariable Long id) {
         return productService.getProduct(id);
     }
 
+    /**
+     * 로그인이 되어있고, 권한이 USER인 경우
+     * 새로운 상품 등록 요청을 처리합니다.
+     * 상품을 등록하면 상태코드 201과 등록된 상품을 응답합니다
+     *
+     * @param productData 등록하고자 하는 상품의 정보
+     * @return 등록된 상품정보
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("isAuthenticated() and hasAuthority('USER')")
@@ -41,7 +66,16 @@ public class ProductController {
         return productService.createProduct(productData);
     }
 
+    /**
+     * 로그인이 되어 있고, 등록된 상품중 id에 해당하는 상품의 정보를 수정하려는 요청을 처리합니다.
+     * 상태코드 200과 수정된 상품 정보를 응답합니다.
+     *
+     * @param id          상품의 id
+     * @param productData 수정하려는 상품 정보
+     * @return 수정된 상품 정보
+     */
     @PatchMapping("{id}")
+    @ResponseStatus(HttpStatus.OK)
     @PreAuthorize("isAuthenticated()")
     public Product update(
             @PathVariable Long id,
@@ -50,7 +84,14 @@ public class ProductController {
         return productService.updateProduct(id, productData);
     }
 
+    /**
+     * 로그인이 되어 있고,  등록된 상품중 id에 해당하는 상품 삭제 요청을 처리합니다
+     * 상태코드 204을 응답합니다
+     *
+     * @param id 상품 id
+     */
     @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @PreAuthorize("isAuthenticated()")
     public void destroy(@PathVariable Long id) {
         productService.deleteProduct(id);
