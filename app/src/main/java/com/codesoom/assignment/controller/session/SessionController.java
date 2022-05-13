116e94a8dc7c917ad5f1292c35ee7c21b25e2ec9
subin606 <gaemilee606@gmail.com>
2022-05-13 10:57:28 +0900

    SessionController javaDoc 작성

--- app/src/main/java/com/codesoom/assignment/controller/session/SessionController.java
+++ app/src/main/java/com/codesoom/assignment/controller/session/SessionController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+/** 회원 로그인 요청을 처리합니다. */
 @CrossOrigin
 @RequestMapping("/session")
 @RestController
