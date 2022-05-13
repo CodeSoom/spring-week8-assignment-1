fc9eceda98949d43780a0910835815194af8cb75
subin606 <gaemilee606@gmail.com>
2022-05-13 11:11:13 +0900

    InvalidTokenException javaDoc 작성

--- app/src/main/java/com/codesoom/assignment/exceptions/InvalidTokenException.java
+++ app/src/main/java/com/codesoom/assignment/exceptions/InvalidTokenException.java
@@ -2,6 +2,7 @@ package com.codesoom.assignment.exceptions;
 
 import org.springframework.http.HttpStatus;
 
+/** 인증 토큰 유효성 검증 실패 시 던집니다. */
 public class InvalidTokenException extends RuntimeException {
 
     private String code;
