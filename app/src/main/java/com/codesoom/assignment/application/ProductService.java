7393c7d4e359bd5dcf2537b603d19052940896ac
yongjunleeme <chesidyong@gmail.com>
2022-06-21 14:32:51 +0900

    상품 서비스에 주석을 작성하라
    상품 서비스의 역할을 설명했습니다.

--- app/src/main/java/com/codesoom/assignment/application/ProductService.java
+++ app/src/main/java/com/codesoom/assignment/application/ProductService.java
@@ -10,6 +10,9 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.List;
 
+/**
+ * 상품 관련 비즈니스 로직을 담당한다.
+ */
 @Service
 @Transactional
 public class ProductService {
@@ -24,20 +27,45 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
+    /**
+     * 모든 상품을 반환한다.
+     */
     public List<Product> getProducts() {
         return productRepository.findAll();
     }
 
-    public Product getProduct(Long id) {
+    /**
+     * 주어진 id에 해당하는 상품을 반환한다.
+     *
+     * @param id 상품의 식별자
+     * @return 주어진 id에 해당하는 상품
+     * @throws ProductNotFoundException 상품을 찾을 수 없는 경우
+     */
+    public Product getProduct(Long id) throws ProductNotFoundException {
         return findProduct(id);
     }
 
+    /**
+     * 주어진 상품 데이터로 상품을 생성하고 상품을 반환한다.
+     *
+     * @param productData 상품 정보
+     * @return 생성한 상품
+     */
     public Product createProduct(ProductData productData) {
         Product product = mapper.map(productData, Product.class);
         return productRepository.save(product);
     }
 
-    public Product updateProduct(Long id, ProductData productData) {
+    /**
+     * 주어진 id에 해당하는 상품을 주어진 상품 데이터로 수정하고 수정한 상품을 반환한다.
+     *
+     * @param id 수정할 상품의 식별자
+     * @param productData 수정할 상품 데이터
+     * @return 수정한 상품
+     * @throws ProductNotFoundException 상품을 찾을 수 없는 경우
+     */
+    public Product updateProduct(Long id, ProductData productData)
+            throws ProductNotFoundException {
         Product product = findProduct(id);
 
         product.changeWith(mapper.map(productData, Product.class));
@@ -45,7 +73,14 @@ public class ProductService {
         return product;
     }
 
-    public Product deleteProduct(Long id) {
+    /**
+     * 주어진 id에 해당하는 상품을 삭제한다.
+     *
+     * @param id 삭제할 상품의 식별자
+     * @return 삭제한 상품
+     * @throws ProductNotFoundException 상품을 찾을 수 없는 경우
+     */
+    public Product deleteProduct(Long id) throws ProductNotFoundException {
         Product product = findProduct(id);
 
         productRepository.delete(product);
