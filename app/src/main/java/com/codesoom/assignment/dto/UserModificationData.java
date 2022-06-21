3992e664ef495fbca015506dc15ad215dd64e39d
seunglo <devseung9056@gmail.com>
2022-06-21 13:09:40 +0900

    dto들의 jackson 매핑을 위한 JsonCreator 어노테이션 추가

--- app/src/main/java/com/codesoom/assignment/dto/UserModificationData.java
+++ app/src/main/java/com/codesoom/assignment/dto/UserModificationData.java
@@ -1,7 +1,8 @@
 package com.codesoom.assignment.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.github.dozermapper.core.Mapping;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,15 +10,21 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Getter
-@Builder
-@AllArgsConstructor
 public class UserModificationData {
     @NotBlank
     @Mapping("name")
-    private String name;
+    private final String name;
 
     @NotBlank
     @Size(min = 4, max = 1024)
     @Mapping("password")
-    private String password;
+    private final String password;
+
+    @JsonCreator
+    @Builder
+    public UserModificationData(@JsonProperty("name") String name,
+                                @JsonProperty("password") String password) {
+        this.name = name;
+        this.password = password;
+    }
 }
