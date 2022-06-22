1171d29d71e5a5a8102079b85ffbf592866279d6
yongjunleeme <chesidyong@gmail.com>
2022-06-22 14:52:29 +0900

    '*'를 사용한 import문을 구체적 import문으로 변경하라

--- app/src/main/java/com/codesoom/assignment/controllers/UserController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/UserController.java
@@ -9,7 +9,14 @@ import com.codesoom.assignment.security.UserAuthentication;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
