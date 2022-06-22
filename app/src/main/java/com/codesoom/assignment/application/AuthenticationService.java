6ece86b395415bbf2fbe33c09f6abe338faf58f0
yongjunleeme <chesidyong@gmail.com>
2022-06-22 14:50:47 +0900

    인증 서비스의 역할을 설명하라

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -4,6 +4,7 @@ import com.codesoom.assignment.domain.Role;
 import com.codesoom.assignment.domain.RoleRepository;
 import com.codesoom.assignment.domain.User;
 import com.codesoom.assignment.domain.UserRepository;
+import com.codesoom.assignment.errors.InvalidTokenException;
 import com.codesoom.assignment.errors.LoginFailException;
 import com.codesoom.assignment.utils.JwtUtil;
 import io.jsonwebtoken.Claims;
@@ -12,6 +13,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * 인증 관련 비즈니스 로직을 담당한다.
+ */
 @Service
 public class AuthenticationService {
     private final UserRepository userRepository;
@@ -29,7 +33,16 @@ public class AuthenticationService {
         this.passwordEncoder = passwordEncoder;
     }
 
-    public String login(String email, String password) {
+    /**
+     * 주어진 이메일과 비밀번호로 로그인하고 생성한 액세스 토큰을 리턴한다.
+     *
+     * @param email    이메일
+     * @param password 비밀번호
+     * @return 생성한 액세스 토큰
+     * @throws LoginFailException 로그인에 실패한 경우
+     */
+    public String login(String email, String password)
+            throws LoginFailException {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new LoginFailException(email));
 
@@ -40,11 +53,25 @@ public class AuthenticationService {
         return jwtUtil.encode(user.getId());
     }
 
-    public Long parseToken(String accessToken) {
+    /**
+     * 주어진 액세스 토큰을 파싱해 파싱한 값을 반환한다.
+     *
+     * @param accessToken 액세스 토큰
+     * @return 파싱한 값
+     * @throws InvalidTokenException 토큰이 유효하지 않은 경우
+     */
+    public Long parseToken(String accessToken)
+            throws InvalidTokenException {
         Claims claims = jwtUtil.decode(accessToken);
         return claims.get("userId", Long.class);
     }
 
+    /**
+     * 주어진 회원 id에 해당하는 모든 권한을 반환한다.
+     *
+     * @param userId 회원 식별자
+     * @return 모든 권한
+     */
     public List<Role> roles(Long userId) {
         return roleRepository.findAllByUserId(userId);
     }
