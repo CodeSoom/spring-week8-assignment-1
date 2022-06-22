38eeb5287340fc816c899c4a03b99d8503e219ea
yongjunleeme <chesidyong@gmail.com>
2022-06-22 15:31:41 +0900

    회원 서비스의 역할을 설명하라

--- app/src/main/java/com/codesoom/assignment/application/UserService.java
+++ app/src/main/java/com/codesoom/assignment/application/UserService.java
@@ -15,6 +15,9 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
+/**
+ * 회원 관련 비즈니스 로직을 담당한다.
+ */
 @Service
 @Transactional
 public class UserService {
@@ -33,7 +36,15 @@ public class UserService {
         this.passwordEncoder = passwordEncoder;
     }
 
-    public User registerUser(UserRegistrationData registrationData) {
+    /**
+     * 주어진 회원 정보로 회원을 생성해 저장하고 생성한 회원을 반환한다.
+     *
+     * @param registrationData 회원 정보
+     * @return 생성한 회원
+     * @throws UserEmailDuplicationException 회원 이메일이 중복인 경우
+     */
+    public User registerUser(UserRegistrationData registrationData)
+            throws UserEmailDuplicationException {
         String email = registrationData.getEmail();
         if (userRepository.existsByEmail(email)) {
             throw new UserEmailDuplicationException(email);
@@ -44,13 +55,25 @@ public class UserService {
 
         user.changePassword(registrationData.getPassword(), passwordEncoder);
 
-        roleRepository.save(new Role(user.getId(), "USER"));
+        roleRepository.save(new Role(user.getId(), "USER" ));
 
         return user;
     }
 
-    public User updateUser(Long id, UserModificationData modificationData,
-                           Long userId) throws AccessDeniedException {
+    /**
+     * 주어진 id에 해당하는 회원을 주어진 수정 정보로 수정하고 수정한 회원을 반환한다.
+     *
+     * @param id 수정할 회원 식별자
+     * @param modificationData 회원 수정 정보
+     * @param userId 인증한 회원 식별자
+     * @return 변경한 회원
+     * @throws AccessDeniedException 변경할 회원이 자신이 아닌 경우
+     * @throws UserNotFoundException 회원을 찾을 수 없는 경우
+     */
+    public User updateUser(Long id,
+                           UserModificationData modificationData,
+                           Long userId)
+            throws AccessDeniedException, UserNotFoundException {
         if (!id.equals(userId)) {
             throw new AccessDeniedException("Access denied");
         }
@@ -63,7 +86,14 @@ public class UserService {
         return user;
     }
 
-    public User deleteUser(Long id) {
+    /**
+     * 주어진 id에 해당하는 회원을 삭제하고 삭제한 회원을 반환한다.
+     *
+     * @param id 삭제할 회원의 식별자
+     * @return 삭제한 회원
+     * @throws UserNotFoundException 회원을 찾을 수 없는 경우
+     */
+    public User deleteUser(Long id) throws UserNotFoundException {
         User user = findUser(id);
         user.destroy();
         return user;
