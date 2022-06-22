e56063c8edb91af304a9e146982ed0a7c42bbc0b
yongjunleeme <chesidyong@gmail.com>
2022-06-22 15:56:15 +0900

    Jwt 활용 클래스를 설명하라

--- app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
+++ app/src/main/java/com/codesoom/assignment/utils/JwtUtil.java
@@ -5,11 +5,15 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.security.SignatureException;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
 
+/**
+ * JWT 토큰의 활용을 담당한다.
+ */
 @Component
 public class JwtUtil {
     private final Key key;
@@ -18,6 +22,12 @@ public class JwtUtil {
         key = Keys.hmacShaKeyFor(secret.getBytes());
     }
 
+    /**
+     * 주어진 회원 id로 부호화한 JWT 토큰 문자열을 반환한다.
+     *
+     * @param userId 부호화할 회원 식별자
+     * @return JWT 토큰
+     */
     public String encode(Long userId) {
         return Jwts.builder()
                 .claim("userId", userId)
@@ -25,8 +35,15 @@ public class JwtUtil {
                 .compact();
     }
 
-    public Claims decode(String token) {
-        if (token == null || token.isBlank()) {
+    /**
+     * 주어진 토큰을 복호화하고 복호화한 정보를 반환한다.
+     *
+     * @param token 복호화할 토큰
+     * @return 토큰에 담긴 정보
+     */
+    public Claims decode(String token)
+            throws InvalidTokenException {
+        if (token == null || StringUtils.isBlank(token)) {
             throw new InvalidTokenException(token);
         }
 
