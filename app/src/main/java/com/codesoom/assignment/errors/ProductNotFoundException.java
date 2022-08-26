eedb305c264b10446a91b98378dbf839444ca2bb
박범진 <qjawlsqjacks@naver.com>
2022-08-26 09:36:22 +0900

    상품을 찾지 못하면 발생하는 예외 핸들링

--- app/src/main/java/com/codesoom/assignment/errors/ProductNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/errors/ProductNotFoundException.java
@@ -1,5 +1,8 @@
 package com.codesoom.assignment.errors;
 
+/**
+ * 상품을 찾지 못하면 던집니다.
+ */
 public class ProductNotFoundException extends RuntimeException {
     public ProductNotFoundException(Long productId) {
         super(String.valueOf(productId));
