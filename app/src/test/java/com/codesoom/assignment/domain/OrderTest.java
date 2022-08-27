b97d4aebafc71facfb2f8c128a6b0b93b80242af
박범진 <qjawlsqjacks@naver.com>
2022-08-27 18:41:06 +0900

    주문 수량이 상품 수량보다 많으면 예외를 던진다.
    예외가 도메인에 종속적일 수 있도록 도메인 내에 정의했습니다.
    RuntimeException을 확장하고 있기에 예외명에서 Exception을 뺐습니다.

--- app/src/test/java/com/codesoom/assignment/domain/OrderTest.java
+++ app/src/test/java/com/codesoom/assignment/domain/OrderTest.java
@@ -21,7 +21,36 @@ public class OrderTest {
         Product product = Fixture.makeProduct(seller);
         // when, then
         assertThatThrownBy(() -> new Order(1L, seller, product, 7))
-                .isExactlyInstanceOf(Order.SellerCannotBuyTheGoodsHeSellsException.class);
+                .isExactlyInstanceOf(Order.IllegalBuyer.class);
+    }
+
+    @Test
+    @DisplayName("구매 수량은 상품의 재고 수량보다 많을 수 없다.")
+    void purchaseQuantityCannotBeGreaterThanStockQuantity() {
+        // given
+        User seller = User.builder()
+                .id(1L)
+                .email(Fixture.ADMIN_EMAIL)
+                .password(Fixture.PASSWORD)
+                .name(Fixture.ADMIN_NAME)
+                .build();
+
+        User buyer = User.builder()
+                .id(2L)
+                .email(Fixture.EMAIL)
+                .password(Fixture.PASSWORD)
+                .name(Fixture.USER_NAME)
+                .build();
+
+        Product product = Product.builder()
+                .owner(seller)
+                .name(Fixture.PRODUCT_NAME)
+                .quantity(10)
+                .price(Fixture.PRICE)
+                .build();
+        // when, then
+        assertThatThrownBy(() -> new Order(1L, buyer, product, 11))
+                .isExactlyInstanceOf(Order.IllegalPurchaseQuantity.class);
     }
 
     static class Order {
@@ -32,19 +61,29 @@ public class OrderTest {
 
         public Order(Long id, User seller, Product product, int quantity) {
             throwIfBuyerAndSellerIsSame(seller, product);
+            throwIfPurchaseQuantityGreaterThanProductStock(product, quantity);
             this.id = id;
             this.seller = seller;
             this.product = product;
             this.quantity = new Quantity(quantity);
         }
 
-        private void throwIfBuyerAndSellerIsSame(User seller, Product product) {
-            if (seller.isSameUser(product.getOwner().getId())) {
-                throw new SellerCannotBuyTheGoodsHeSellsException();
+        private void throwIfPurchaseQuantityGreaterThanProductStock(Product product, int quantity) {
+            if (product.getQuantity() < quantity) {
+                throw new IllegalPurchaseQuantity();
             }
         }
 
-        private static class SellerCannotBuyTheGoodsHeSellsException extends RuntimeException {
+        private void throwIfBuyerAndSellerIsSame(User seller, Product product) {
+            if (seller.isSameUser(product.getOwner().getId())) {
+                throw new IllegalBuyer();
+            }
+        }
+
+        private static class IllegalBuyer extends RuntimeException {
+        }
+
+        public static class IllegalPurchaseQuantity extends RuntimeException {
         }
     }
 }
