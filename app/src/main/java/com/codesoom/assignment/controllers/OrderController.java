ac594ae6567c9396e996aa5792735b115373addb
BEOMKING <qjawlsqjacks@naver.com>
2022-08-28 20:32:57 +0900

    주문 생성 요청 API 구현

--- app/src/main/java/com/codesoom/assignment/controllers/OrderController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/OrderController.java
@@ -1,8 +1,11 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.application.OrderService;
 import com.codesoom.assignment.dto.OrderData;
 import com.codesoom.assignment.dto.OrderInfo;
+import com.codesoom.assignment.utils.SecurityUtil;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -11,8 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public OrderInfo create(@RequestBody OrderData orderData) {
-        return null;
+        return OrderInfo.from(orderService.create(orderData, SecurityUtil.getInfo()));
     }
 }
