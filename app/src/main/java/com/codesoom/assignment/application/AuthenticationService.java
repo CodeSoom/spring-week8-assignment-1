d318c5ec176a4093c79c8a833f3bac8d1c65c1f9
luckycw87 <blissful.pcw@gmail.com>
2022-11-01 15:27:31 +0900

    Add: Service 클래스 JavaDoc 주석 추가

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -12,6 +12,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * Authentication(인증) 관련 비즈니스 로직
+ */
 @Service
 public class AuthenticationService {
     private final UserRepository userRepository;
@@ -29,6 +32,12 @@ public class AuthenticationService {
         this.passwordEncoder = passwordEncoder;
     }
 
+    /**
+     * 로그인 정보로 인증 후 액세스 토큰을 반환한다.
+     * @param email 로그인 이메일
+     * @param password 로그인 비밀번호
+     * @return 액세스 토큰
+     */
     public String login(String email, String password) {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new LoginFailException(email));
@@ -40,11 +49,21 @@ public class AuthenticationService {
         return jwtUtil.encode(user.getId());
     }
 
+    /**
+     * 액세스 토큰을 받아서 디코딩 후 사용자 ID를 리턴한다.
+     * @param accessToken 액세스 토큰
+     * @return 사용자 ID
+     */
     public Long parseToken(String accessToken) {
         Claims claims = jwtUtil.decode(accessToken);
         return claims.get("userId", Long.class);
     }
 
+    /**
+     * 사용자 ID의 사용자가 가지고 있는 사용자 권한을 리턴한다.
+     * @param userId 사용자 ID
+     * @return 사용자 권한 리스트
+     */
     public List<Role> roles(Long userId) {
         return roleRepository.findAllByUserId(userId);
     }
