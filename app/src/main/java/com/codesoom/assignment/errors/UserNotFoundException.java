3ad86086105c8b6e08c9a663e6f5a755563b84e9
luckycw87 <blissful.pcw@gmail.com>
2022-11-01 15:27:53 +0900

    Add: Exception 클래스 JavaDoc 주석 추가

--- app/src/main/java/com/codesoom/assignment/errors/UserNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/errors/UserNotFoundException.java
@@ -1,5 +1,8 @@
 package com.codesoom.assignment.errors;
 
+/**
+ * 사용자가 존재하지않은 경우 던집니다.
+ */
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(Long id) {
         super("User not found: " + id);
