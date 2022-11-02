db797c34b87afa2e15b76221314f8313426ebd81
Eunha Jeong <eunhaay@gmail.com>
2022-11-02 17:23:00 +0900

    예외 설명이 구체적이지 않도록 수정

--- app/src/main/java/com/codesoom/assignment/errors/LoginFailException.java
+++ app/src/main/java/com/codesoom/assignment/errors/LoginFailException.java
@@ -1,7 +1,7 @@
 package com.codesoom.assignment.errors;
 
 /**
- * 이메일을 찾지 못했거나 비밀번호가 일치하지 않는 경우 던집니다.
+ * 로그인 처리에 실패할 경우 던집니다.
  */
 public class LoginFailException extends RuntimeException {
     public LoginFailException(String email) {
