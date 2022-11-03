cdc3cb3cfc368a4832f20b5429df3e3f102e35e6
Eunha Jeong <eunhaay@gmail.com>
2022-11-03 18:14:27 +0900

    API 문서의 Host 설정 정보 수정

--- app/src/test/java/com/codesoom/assignment/support/RestDocsConfig.java
+++ app/src/test/java/com/codesoom/assignment/support/RestDocsConfig.java
@@ -6,6 +6,8 @@ import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.restdocs.operation.preprocess.Preprocessors;
 
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyUris;
+
 @TestConfiguration
 public class RestDocsConfig {
 
@@ -13,7 +15,12 @@ public class RestDocsConfig {
     public RestDocumentationResultHandler write() {
         return MockMvcRestDocumentation.document(
                 "{class-name}/{method-name}", // 스니펫이 생성되는 경로를 테스트를 수행하는 클래스/메서드명으로 정합니다.
-                Preprocessors.preprocessRequest(Preprocessors.prettyPrint()), // 요청을 예쁘게 출력합니다.
+                Preprocessors.preprocessRequest(
+                        Preprocessors.prettyPrint(),  // 요청을 예쁘게 출력합니다.
+                        modifyUris()
+                                .host("api.toy-shop.com") // Host URL을 변경합니다.
+                                .removePort() // Host에서 포트 번호를 숨깁니다.
+                        ),
                 Preprocessors.preprocessResponse(Preprocessors.prettyPrint()) // 응답을 예쁘게 출력합니다.
         );
     }
