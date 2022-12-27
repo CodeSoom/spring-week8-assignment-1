026600b13c5941d6afefc5f3a92800d3b76ba165
EuijeongHwang_Mac <euijeong.hwang97@gmail.com>
2022-12-27 21:18:10 +0900

    Style: AuthenticationService API 주석 작성
    AuthenticationService API 문서를 위한 주석 작성함

--- app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
+++ app/src/main/java/com/codesoom/assignment/application/AuthenticationService.java
@@ -12,13 +12,53 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * The {@code AuthenticationService} class represents the Authentication service.
+ * The class {@code AuthenticationService} includes methods for
+ * encoding and decoding JSON Web Token to authenticate users and getting roles by the user's id.
+ *
+ * @see org.springframework.stereotype.Service
+ */
 @Service
 public class AuthenticationService {
+
+    /**
+     * Repository for User Domain.
+     */
     private final UserRepository userRepository;
+
+    /**
+     * Repository for Role Domain.
+     */
     private final RoleRepository roleRepository;
+
+    /**
+     * The {@code JwtUtil} class is a utility class that encodes and decodes JSON Web Token.
+     */
     private final JwtUtil jwtUtil;
+
+    /**
+     * Service interface for encoding passwords. It implements BCryptPasswordEncoder.
+     *
+     * @see org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+     */
     private final PasswordEncoder passwordEncoder;
 
+    /**
+     * Constructs a new {@code AuthenticationService} by userRepository, roleRepository, jwtUtil and the passwordEncoder.
+     *
+     * @param userRepository
+     *        The repository of the user
+     *
+     * @param roleRepository
+     *        The repository of the role
+     *
+     * @param jwtUtil
+     *        The utility class about Json Web Token
+     *
+     * @param passwordEncoder
+     *        The Bean of PasswordEncoder
+     */
     public AuthenticationService(UserRepository userRepository,
                                  RoleRepository roleRepository,
                                  JwtUtil jwtUtil,
@@ -29,6 +69,19 @@ public class AuthenticationService {
         this.passwordEncoder = passwordEncoder;
     }
 
+    /**
+     * Returns jwt of the user's id.
+     *
+     * @param email
+     *        The email address of the user
+     *
+     * @param password
+     *        The password of the user
+     *
+     * @return the jwt token created through by the user's id
+     *
+     * @throws LoginFailException if you can't find the user by email or the password doesn't match.
+     */
     public String login(String email, String password) {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new LoginFailException(email));
@@ -40,11 +93,27 @@ public class AuthenticationService {
         return jwtUtil.encode(user.getId());
     }
 
+    /**
+     * Returns user's id by decoded jwt.
+     *
+     * @param accessToken
+     *        The jwt by the user's id
+     *
+     * @return the user's id through by decoded jwt
+     */
     public Long parseToken(String accessToken) {
         Claims claims = jwtUtil.decode(accessToken);
         return claims.get("userId", Long.class);
     }
 
+    /**
+     * Returns List of roles by user's id.
+     *
+     * @param userId
+     *        The id of the user
+     *
+     * @return The list of roles by user's id
+     */
     public List<Role> roles(Long userId) {
         return roleRepository.findAllByUserId(userId);
     }
