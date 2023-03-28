9d987fc003eefd231101947120ec2a57e49c6533
tlatmsrud <tlatmsrud@naver.com>
2023-03-28 13:18:11 +0900

    SpringSecurityJavaConfig에 대한 주석 추가

--- app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
+++ app/src/main/java/com/codesoom/assignment/config/SecurityJavaConfig.java
@@ -20,6 +20,19 @@ public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private AuthenticationService authenticationService;
 
+    /**
+     * 스프링 시큐리티 설정
+     * csrf disable - CSRF Token 검증 방식 비활성화
+     * headers.frameOptions.disable - click jacking 방어를 위한 기본 frameOption 비활성화.
+     * addFilter(authenticationFilter) - JWT 검증 필터 추가
+     * addFilterBefore - JwtAuthenticationFilter 이전 필터에 authenticationErrorFilter 필터 추가 
+     * SessionCreationPolicy.STATELESS - 세션을 통한 인증 정책 비활성화 (JWT 를 사용하기 때문)
+     * exceptionHandling - 인증 및 인가예외 발생 시 401 에러 응답
+     * @EnableGlobalMethodSecurity(prePostEnabled = true) - 어노테이션 기반 메서드 보안 기능 활성화 (Controller의 @PreAuthorize 사용 중)
+     *
+     * @param http the {@link HttpSecurity} to modify
+     * @throws Exception
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         Filter authenticationFilter = new JwtAuthenticationFilter(
